22567edb287a5d061865d301b99db69dbe843957
hvenses <hvenses@gmail.com>
2021-05-03 20:21:06 -0300

    Configuracao de back-end/login/token

--- back-end/controllers/loginController.js
+++ back-end/controllers/loginController.js
@@ -5,14 +5,14 @@ const secret = 'segredo';
 
 const generateToken = (result, jwtConfig) => {
   const { email, role, id } = result;
-  const token = jwt.sign({ email, role, id }, jwtConfig, secret);
+  const token = jwt.sign({ email, role, id }, secret, jwtConfig);
   return token;
 };
 
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const result = await loginService.validations(email, password);
+    const [result] = await loginService.validations(email, password);
     if (result) {
       const jwtConfig = {
         expiresIn: 60 * 60,
@@ -32,22 +32,22 @@ module.exports = {
   login,
 };
 
-const checkLogin = async (request, response) => {
-  try {
-    const { email, password } = request.body;
-    const result = await serviceLogin.validations(email, password);
-    if (result) {
-      const jwtConfig = {
-        expiresIn: 60 * 60,
-        algorithm: 'HS256',
-      };
-      const isMath = bcrypt.compareSync(password, result.password);
-      if (!isMath) throw new Error('password invalid!!');
-      const token = fnGenerateToken(result, jwtConfig);
-      return response.status(OK).json({ token });
-    }
-  } catch (error) {
-    const { message } = error;
-    return response.status(ERROR).json({ message });
-  }
-};
+// const checkLogin = async (request, response) => {
+//   try {
+//     const { email, password } = request.body;
+//     const result = await serviceLogin.validations(email, password);
+//     if (result) {
+//       const jwtConfig = {
+//         expiresIn: 60 * 60,
+//         algorithm: 'HS256',
+//       };
+//       const isMath = bcrypt.compareSync(password, result.password);
+//       if (!isMath) throw new Error('password invalid!!');
+//       const token = fnGenerateToken(result, jwtConfig);
+//       return response.status(OK).json({ token });
+//     }
+//   } catch (error) {
+//     const { message } = error;
+//     return response.status(ERROR).json({ message });
+//   }
+// };
