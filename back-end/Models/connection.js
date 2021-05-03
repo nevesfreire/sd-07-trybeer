89003c7389163de36c1b7db3fe79c59ce10005fa
vitorAmorims <vtamorims@gmail.com>
2021-05-03 17:07:57 -0300

    config inicial express

--- back-end/Models/connection.js
+++ back-end/Models/connection.js
@@ -1,11 +1,11 @@
-const  mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
-const conn = mysql.createPool({
+const connection = mysql.createPool({
     host: process.env.HOSTNAME,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: 'Trybeer',
-    port: '3306'
+    port: '3306',
   });
   
   module.exports = connection;
