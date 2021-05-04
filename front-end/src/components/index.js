2e3c28fbf54d0ad880ee09297980a7ec066dc39f
Lucas <luc.zago@hotmail.com>
2021-05-04 13:32:51 -0300

    Implementado redux na aplicação

---
+++ front-end/src/components/index.js
@@ -0,0 +1,5 @@
+import Routes from './Routes.jsx';
+
+export {
+  Routes,
+};
---
+++ front-end/src/services/api.js
@@ -0,0 +1,13 @@
+import { getStorage } from './localSorage'
+
+const URL = process.env.REACT_APP_ENDPOINT;
+
+const tokenKeyStorage = 'token';
+
+const ENDPOINT = {
+  // levels: '/level',
+  // login: '/login',
+  // event: '/event',
+  // user: '/user',
+  // token: '/oauth/token'
+}
