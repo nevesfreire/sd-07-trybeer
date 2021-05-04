e2fe1b20a47067e8f1ae368c79189e8c231061b6
cezar88augusto <cezar88augusto@gmail.com>
2021-05-04 16:50:42 -0300

    Requisito 2 Front-end Finalizado

---
+++ front-end/src/pages/Register/Register.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom';
+
+// nome - deve conter, no mínimo, 12 letras, sem números ou caracteres especiais;
+
+// email - deve conter um email válido. Um email válido possui o formato <nome>@<domínio>. 
+// Não deve ser possível cadastrar mais de um usuário com o mesmo e-mail, nesse caso a aplicação deve exibir a mensagem Já existe um usuário com esse e-mail.;
+
+// senha - composta por, no mínimo, 6 caracteres;
+
+// quero vender - um checkbox opcional, desmarcado por padrão.
+
+function Register () {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSeller, setIsSeller] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => {
+    const regexString = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
+    const regexEmail = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    const MIN_LENGTH = 6;
+    const MIN_NAME = 12;
+    if(password.length >= MIN_LENGTH && name.length > MIN_NAME && regexString.test(name) && regexEmail.test(email)){
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [name, email, password])
+
+  function handleChange({ target }) {
+    switch(target.id) {
+      case 'name-input':
+        return setName(target.value);
+      case 'email-input':
+        return setEmail(target.value);
+      case 'password-input':
+        return setPassword(target.value);
+      default:
+        return;
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    return history.push('/products');
+  }
+
+  return (
+    <div>
+      <h2>Página de registro</h2>
+      <form>
+        <label htmlFor="name-input">
+          <h6>Nome</h6>
+          <input 
+            type="text"
+            id="name-input"
+            data-testid="signup-name"
+            onChange={ handleChange } 
+            value={ name } 
+          />
+        </label>
+        <label htmlFor="email-input">
+          <h6>Email</h6>
+          <input
+            type="email"
+            id="email-input"
+            data-testid="signup-email"
+            onChange={ handleChange }
+            value={ email } 
+          />
+        </label>
+        <label htmlFor="password-input">
+          <h6>Senha</h6>
+          <input 
+            type="password"
+            id="password-input"
+            data-testid="signup-password"
+            onChange={ handleChange }
+            value={ password }
+          />
+        </label>
+        <label htmlFor="checkbox-input">
+          <input 
+            type="checkbox"
+            id="checkbox-input"
+            data-testid="signup-seller"
+            onClick={ () => setIsSeller(!isSeller) } 
+            checked={ isSeller } 
+          />
+          Quero vender
+        </label>
+        <button 
+            type="submit"
+            data-testid="signup-btn"
+            onClick={ handleSubmit } 
+            disabled={ disabled }
+        >
+            Cadastrar
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Register;
