ab1ce03614eaac3f6bc99dce2d1a2051b54df46d
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-05-04 19:45:24 -0300

    front-req-1

--- front-end/src/context/index.js
+++ front-end/src/context/index.js
@@ -1,13 +1,16 @@
 import React, { createContext } from 'react';
+import PropTypes from 'prop-types';
 
 const Context = createContext();
 
-const Provider = ({ children }) => {
-  return (
-    <Context.Provider value={{}}>
-      {children}
-    </Context.Provider>
-  );
+const Provider = ({ children }) => (
+  <Context.Provider value={ {} }>
+    {children}
+  </Context.Provider>
+);
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default { Context, Provider };
