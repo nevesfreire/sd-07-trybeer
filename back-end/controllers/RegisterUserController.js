bd05c879ed191f4da24c91c9c83f8d1f40755f11
Éric Vinícius <ericcobain3@gmail.com>
2021-05-04 18:51:53 -0300

    requisito-back-end

--- back-end/controllers/RegisterUserController.js
+++ back-end/controllers/RegisterUserController.js
@@ -4,7 +4,7 @@ const registerUser = async (req, res) => {
   const { name, email, password, wantToSell } = req.body;
   
   const result = await User.registerUser(name, email, password, wantToSell);
-  if (result) return res.status(400).json(result);  
+  // if (result) return res.status(400).json(result);  
 
   return res.status(201).json(result);
 };
--- back-end/services/registerUserService.js
+++ back-end/services/registerUserService.js
@@ -1,50 +1,50 @@
 const User = require('../models/UserModel');
 
-const EmailAlreadyExists = {
-  message: 'Já existe um usuário com esse e-mail.',
-};
-const validateName = (name) => {
-  const reg = /^[a-z\s]+$/i;
-  if (!name) return { message: 'O campo name é obrigatório' };
-  if (name.length < 12) {
-    return { message: 'O name deve ter pelo menos 12 caracteres' }; 
-  }
-  if (!reg.test(name)) {
-    return { message: 'O nome nao deve ter números ou caracteres especiais' };
-  } 
-}; 
+// const EmailAlreadyExists = {
+//   message: 'Já existe um usuário com esse e-mail.',
+// };
+// const validateName = (name) => {
+//   const reg = /^[a-z\s]+$/i;
+//   if (!name) return { message: 'O campo name é obrigatório' };
+//   if (name.length < 12) {
+//     return { message: 'O name deve ter pelo menos 12 caracteres' }; 
+//   }
+//   if (!reg.test(name)) {
+//     return { message: 'O nome nao deve ter números ou caracteres especiais' };
+//   } 
+// }; 
 
-const validateEmail = (email) => {
-  const reg = /\S+@\S+\.\S+/;
-  if (!reg.test(email)) return { message: 'Email inválido' };
-}; 
-const validateIfEmailExist = async (email) => {
-  const result = await User.getByEmail(email);
-  if (result !== null) return EmailAlreadyExists;
-}; 
-const validatePassword = (password) => {
-  const passwordParsed = password.toString();    
-  const arrayPassword = passwordParsed.split('');
-  if (arrayPassword.length < 6) return { message: 'Senha deve ter no mínimo 6 caracters' };  
-};  
-const validateFields = async (name, email, password) => {
-  const result = await validateIfEmailExist(email);
-  if (validateName(name)) {
-    return validateName(name);
-  } 
+// const validateEmail = (email) => {
+//   const reg = /\S+@\S+\.\S+/;
+//   if (!reg.test(email)) return { message: 'Email inválido' };
+// }; 
+// const validateIfEmailExist = async (email) => {
+//   const result = await User.getByEmail(email);
+//   if (result !== null) return EmailAlreadyExists;
+// }; 
+// const validatePassword = (password) => {
+//   const passwordParsed = password.toString();    
+//   const arrayPassword = passwordParsed.split('');
+//   if (arrayPassword.length < 6) return { message: 'Senha deve ter no mínimo 6 caracters' };  
+// };  
+// const validateFields = async (name, email, password) => {
+//   const result = await validateIfEmailExist(email);
+//   if (validateName(name)) {
+//     return validateName(name);
+//   } 
 
-  if (!validateEmail(email)) return validateEmail(email);
+//   if (!validateEmail(email)) return validateEmail(email);
 
-  if (validatePassword(password)) return validatePassword(password);
+//   if (validatePassword(password)) return validatePassword(password);
 
-  if (result.message) {
-    return EmailAlreadyExists;  
-  } 
-};
+//   if (result.message) {
+//     return EmailAlreadyExists;  
+//   } 
+// };
 const registerUser = async (name, email, password, wantToSell) => {
   let role = 'client';
-  const message = await validateFields(name, email, password);
-  if (message) return message;
+  // const message = await validateFields(name, email, password);
+  // if (message) return message;
 
   if (wantToSell) role = 'admin';
 
