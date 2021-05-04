53af8762b89c16ae2fad1ef4b39a77780f2a10e7
paula-jfe <paula.jfe@gmail.com>
2021-05-04 18:01:37 -0300

    eslint parte 2

--- back-end/configuration/configuration.js
+++ back-end/configuration/configuration.js
@@ -1,13 +1,14 @@
-require('dotenv').config();;
+require('dotenv').config();
+
 // get the client
 const mysql = require('mysql2');
- 
+
 // create the connection to database
 const connection = mysql.createConnection({
   host: process.env.HOSTNAME,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'Trybeer'
+  database: 'Trybeer',
 });
 
 module.exports = connection;
