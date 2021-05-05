b16e2c454aff4c647950c1756dca53b1aacb89f7
Lfocosi <luccafocosi@gmail.com>
2021-05-05 18:06:32 -0300

    rotatas e views

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import { SideBar } from '../components';
-
 import Views from '../views';
 
 export default function Routes() {
@@ -12,8 +10,16 @@ export default function Routes() {
       <Switch>
         <Route exact path="/login" component={ Views.Login } />
         <Route exact path="/register" component={ Views.Register } />
-        <Route exact path="/SideBar" component={ <SideBar title="Trybeer" /> } />
+        <Route exact path="/profile" component={ Views.Profile } />
+        <Route exact path="/products" component={ Views.Products } />
+        <Route exact path="/checkout" component={ Views.Checkout } />
+        <Route exact path="/orders" component={ Views.Orders } />
+        <Route exact path="/admin/profile" component={ Views.AdminProfile } />
+        <Route exact path="/admin/orders" component={ Views.AdminOrders } />
+        <Route exact path="/orders/:id" component={ Views.OrdersID } />
+        <Route exact path="/admin/orders/:id" component={ Views.AdminOrdersID } />
         <Route exact path="/" component={ () => <Redirect to="/login" /> } />
+        <Route path="/" component={ () => <h1>desculpe amigo, não tem nada aqui</h1> } />
       </Switch>
     </BrowserRouter>
   );
