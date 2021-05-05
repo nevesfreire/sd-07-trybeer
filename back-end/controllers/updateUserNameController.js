0d60b7b3b1a4168e6b8a602827c5778a046a5ba7
Éric Vinícius <ericcobain3@gmail.com>
2021-05-04 21:55:05 -0300

    mudei o status http de resposta

--- back-end/controllers/updateUserNameController.js
+++ back-end/controllers/updateUserNameController.js
@@ -1,13 +1,13 @@
 const User = require('../services/updateUserNameService');
 
-const UPDATED = 200;
+const OK = 200;
 
 const updateUserName = async (req, res) => {
   const { name, email } = req.body;
   
   const result = await User.updateUserName(name, email);
 
-  return res.status(UPDATED).json(result);
+  return res.status(OK).json(result);
 };
 
 module.exports = {
