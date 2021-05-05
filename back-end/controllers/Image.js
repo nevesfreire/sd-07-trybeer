ee651f4f3d08306e8077bdc77815762f8738cd93
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-05-05 15:22:52 -0300

    Alterando o nome da imagem para teste

--- back-end/controllers/Image.js
+++ back-end/controllers/Image.js
@@ -1,6 +1,6 @@
 const getImage = async (req, res) => {
   try {
-    res.sendFile(req.url, { root: '../' });
+    res.sendFile(req.url, { root: './' });
   } catch (error) {
     res.status(500).json(error.message);
   }
