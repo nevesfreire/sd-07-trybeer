eb44c35ed0a2ddf581ac0099571bd9ddf67b2c4a
dennerpl <denner.souzapl@gmail.com>
2021-05-05 17:34:50 -0300

    retornando o token e o role

--- back-end/controller/usersController.js
+++ back-end/controller/usersController.js
@@ -1,11 +1,25 @@
 const usersService = require('../service/usersService');
+const jtw = require('jsonwebtoken');
 
 const findUserByEmail = async (req, res) => {
-  // const { email } = req.body;
-  // const user = await usersService.findByEmail(email);
+  const { email, password } = req.body;
+  const user = await usersService.findByEmail(email);
+  if(user === undefined || user.password !== password ) {
+    return res.status(404).json({
+      message: 'usuário ou senha incorreto'
+    })
+  }
+  const userJWT = {
+    email: user.email,
+    role: user.role,
+  }
+  const token = usersService.generateToken(userJWT);
 
   try {
-    return res.status(200).json({ message: 'aqui' });
+    return res.status(200).json({ 
+      token,
+      role: user.role
+    });
   } catch (error) {
     return res.status(500).json(error.message);
   }
