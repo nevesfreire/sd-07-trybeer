b2f8a7fb491f99548aa7da5efd24934de289331b
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-05-05 16:43:41 -0300

    alterando Content Password para Senha

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -37,7 +37,7 @@ const ComponentLogin = () => {
             />
           </label>
           <label htmlFor="password">
-            Password
+            Senha
             <input
               data-testid="password-input"
               id="password"
