16379ada6b5e29932b87b6f00ccb44d30d30c2fe
Lucas <luc.zago@hotmail.com>
2021-05-05 09:19:15 -0300

    Adicionando Card e Products

---
+++ front-end/src/components/Card.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useDispatch } from "react-redux";
+import { getStorage, setStorage } from '../services/localStorage';
+import { changeTotalPrice } from '../actions';
+
+function Card(product) {
+  const ZERO = 0;
+  const ONE = 1;
+
+  const [quantity, setQuantity] = useState(ZERO);
+
+  const dispatch = useDispatch();
+  
+  const { url_image, name, price } = product;
+
+  const setCart = () => {
+    if (!getStorage('cart')) setStorage('cart', {});
+    const cart = getStorage('cart');
+    const totalProductPrice = price * quantity;
+    cart[`${name}`] = { quantity, totalProductPrice };
+    cart.totalPrice += totalProductPrice;
+    dispatch(changeTotalPrice(cart.totalPrice));
+    setStorage('cart', cart);
+  }
+
+  const changeQuantity = (operation) => {
+    if (operation === 'minus' && quantity > ZERO) {
+      setQuantity(quantity - ONE);
+      setCart();
+    }
+    if (operation === 'plus') {
+      setQuantity(quantity + ONE);
+      setCart();
+    }
+  }
+
+  return (
+    <div>
+      <img src={ url_image } />
+      <h5>{ price }</h5>
+      <h6>{ name }</h6>
+      <button
+        type="button"
+        onClick={ changeQuantity("plus") }
+      >
+        -
+      </button>
+      <span>{ quantity }</span>
+      <button
+        type="button"
+        onClick={ changeQuantity("minus") }
+      >
+        +
+      </button>
+    </div>
+  )
+}
+
+export default Card;
---
+++ front-end/src/pages/Products/Products.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+import { Card } from '../../components';
+import * as API from '../../services/api';
+import { getStorage } from '../../services/localStorage';
+
+function Products() {
+  useEffect(() => {
+    if (!getStorage('cart')) {
+      // chama api
+    }
+  }, []);
+
+  const totalPrice = useSelector((state) => state.clientInfo.totalPrice);
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Products;
