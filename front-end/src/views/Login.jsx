5dbf186691f1ea461289bc206ddbe793c3890050
Felipe Nascimento <flpnascto@gmail.com>
2021-05-06 19:39:59 -0300

    requisito 02 frontend

--- front-end/src/views/Login.jsx
+++ front-end/src/views/Login.jsx
@@ -1,15 +1,26 @@
 import React, { useCallback, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { EmailInput, PasswordInput } from '../components';
 import { userDataValidation } from '../utils';
+import fetchApi from '../hooks/fetchApi';
+import CODE from '../utils/statusCode';
 
 const validation = ({ email, password }) => (
   userDataValidation.email(email) && userDataValidation.password(password)
 );
 
+const handleLogin = async (body, history) => {
+  const login = await fetchApi('/login', 'POST', body);
+  if (login.statusCode === CODE.OK) {
+    window.localStorage.setItem('user', JSON.stringify(login.user));
+    if (login.user.role === 'administrator') return history.push('/admin/orders');
+    history.push('/products');
+  }
+};
+
 export default function Login() {
   const [user, setUser] = useState({});
-
+  const history = useHistory();
   const handleState = useCallback(({ target: { value, id } }) => {
     setUser((state) => ({ ...state, [id]: value }));
   }, []);
@@ -28,11 +39,12 @@ export default function Login() {
         data-testid="signin-btn"
         type="button"
         disabled={ !validation(user) }
+        onClick={ () => handleLogin(user, history) }
       >
         Entrar
       </button>
 
-      <Link data-testid="no-account-btn" to="/register">Ainda nao tenho conta</Link>
+      <Link data-testid="no-account-btn" to="/register">Ainda não tenho conta</Link>
 
     </div>
   );
--- front-end/src/views/Register.jsx
+++ front-end/src/views/Register.jsx
@@ -1,21 +1,42 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import { EmailInput, PasswordInput, NameInput } from '../components';
+import fetchApi from '../hooks/fetchApi';
 import { userDataValidation } from '../utils';
+import CODE from '../utils/statusCode';
 
 const validation = ({ email, password, name }) => (
   userDataValidation.email(email)
-    && userDataValidation.password(password)
-    && userDataValidation.name(name)
+  && userDataValidation.password(password)
+  && userDataValidation.name(name)
 );
 
 export default function Register() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({
+    nome: '',
+    email: '',
+    password: '',
+    role: false,
+  });
 
-  const handleState = useCallback(({ target: { id, value } }) => {
-    setUser((state) => ({ ...state, [id]: id === 'admin' ? !state.admin : value }));
-  }, []);
+  const [erroMessage, setErrorMessage] = useState(false);
 
-  console.log(validation(user));
+  const history = useHistory();
+
+  const handleState = ({ target: { id, value } }) => {
+    setUser((state) => ({ ...state, [id]: id === 'role' ? !state.role : value }));
+  };
+
+  const handleRegister = async () => {
+    const register = await fetchApi('/register', 'POST', user);
+    console.log('register', register);
+    if (register.statusCode === CODE.CREATED) {
+      window.localStorage.setItem('user', JSON.stringify(register.user));
+      if (register.user.role === 'administrator') return history.push('/admin/orders');
+      return history.push('/products');
+    }
+    setErrorMessage(true);
+  };
 
   return (
     <form>
@@ -36,17 +57,22 @@ export default function Register() {
           type="checkbox"
           onChange={ handleState }
           data-testid="signup-seller"
-          id="admin"
+          id="role"
         />
         Quero vender
       </label>
 
       <button
         data-testid="signup-btn"
-        type="submit"
+        type="button"
+        disabled={ !validation(user) }
+        onClick={ handleRegister }
       >
         Cadastrar
       </button>
+      <span visibility={ erroMessage ? 'hidden' : 'visible' }>
+        Já existe um usuário com esse e-mail.
+      </span>
     </form>
   );
 }
