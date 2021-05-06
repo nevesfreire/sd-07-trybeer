f4f0b085c469cd2fc598e0e0a6fd308055cb88f4
Felipe Nascimento <flpnascto@gmail.com>
2021-05-06 19:37:36 -0300

    erequisito 02 backend

--- back-end/controllers/loginController.js
+++ back-end/controllers/loginController.js
@@ -2,10 +2,10 @@ const User = require('../services/UserService');
 
 module.exports = async (req, res, next) => {
   const { email, password } = req.body;
-
+  console.log('email', email);
   try {
     const { statusCode, user } = await User.findByEmailAndPassword(email, password);
-    res.status(statusCode).json({ ...user, statusCode });
+    res.status(statusCode).json({ user, statusCode });
   } catch (error) {
     next(error);
   }
--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const errorController = require('./controllers/errorController');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const { PORT } = process.env;
