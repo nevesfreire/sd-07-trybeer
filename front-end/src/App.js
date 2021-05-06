4f349fc600d07824f262e9fe87a4a30771af39d7
Francisco <franciscoliratorres@gmail.com>
2021-05-06 17:04:50 -0300

    Req 3 finalizado, faltando ajustes css e 1 lint que nao consegui corrigir

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,8 @@ import Register from './pages/Register';
 import './App.css';
 import Products from './pages/Products';
 import AdminProfile from './pages/Admin/Profile';
+import ClientProfile from './pages/Client/Profile';
+import Orders from './pages/Client/Orders';
 
 const App = () => (
   <BrowserRouter>
@@ -13,6 +15,8 @@ const App = () => (
     <Route exact path="/login" component={ Login } />
     <Route exact path="/register" component={ Register } />
     <Route exact path="/products" component={ Products } />
+    <Route exact path="/orders" component={ Orders } />
+    <Route exact path="/profile" component={ ClientProfile } />
     <Route exact path="/admin/orders" component={ AdminProfile } />
   </BrowserRouter>
 );
---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Hamburger from 'hamburger-react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  const [sideBar, setSideBar] = useState(false);
+
+  const logout = () => {
+    localStorage.setItem('token', JSON.stringify(''));
+    localStorage.setItem('name', JSON.stringify(''));
+    localStorage.setItem('role', JSON.stringify(''));
+    localStorage.setItem('email', JSON.stringify(''));
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <button
+          type="submit"
+          onClick={ () => setSideBar(!sideBar) }
+          data-testid="top-hamburguer"
+          className="hameburgerButton"
+        >
+          <Hamburger easing="ease-in" />
+        </button>
+        <h2 data-testid="top-title">TryBeer</h2>
+      </div>
+      {sideBar
+      && <div>
+        <div className="side-menu-container">
+          <div>
+            <Link
+              className="links"
+              to="/products"
+              data-testid="side-menu-item-products"
+            >
+              Produtos
+            </Link>
+          </div>
+          <div>
+            <Link
+              className="links"
+              to="/orders"
+              data-testid="side-menu-item-my-orders"
+            >
+              Meus Pedidos
+            </Link>
+          </div>
+          <div>
+            <Link
+              className="links"
+              to="/profile"
+              data-testid="side-menu-item-my-profile"
+            >
+              Meu Perfil
+            </Link>
+          </div>
+          <div>
+            <button
+              type="submit"
+              onClick={ () => logout() }
+            >
+              <Link
+                to="/login"
+                data-testid="side-menu-item-logout"
+                className="links"
+              >
+                Sair
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>}
+    </div>
+  );
+};
+
+export default Header;
