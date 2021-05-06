19c22ccaf5e273636420efaaf12fcd7b93b54dda
Vanessa Oliveira <vanessanaara@gmail.com>
2021-05-06 17:40:31 -0300

    Adicionando arquivos

---
+++ front-end/src/pages/AdminHome/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AdminHome = () => (
+  <>
+    <h1>Admin Home</h1>
+  </>
+);
+
+export default AdminHome;
---
+++ front-end/src/pages/Products/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Products = () => (
+  <>
+    <h1>Produtos</h1>
+  </>
+);
+
+export default Products;
