18b81ab26a8ea1b9b153532b0d845b2e2e5a1874
Vitor Rodrigues <vitor_rc@outlook.com>
2021-05-06 16:43:57 -0300

    Ajustes para passar no Req 5

--- front-end/src/components/Products.js
+++ front-end/src/components/Products.js
@@ -3,117 +3,132 @@ import { useHistory } from 'react-router-dom';
 import { fetchProductList } from '../services/api';
 
 function Products() {
-    const zero = 0;
-    const history = useHistory();
-    const [productList, setProductList] = useState([]);
-    const [valorTotal, setValorTotal] = useState(0);
+  const zero = 0;
+  const history = useHistory();
+  const [productList, setProductList] = useState([]);
+  const [valorTotal, setValorTotal] = useState('0,00');
 
-    useEffect(() => {
-        async function didMount() {
-            const user = JSON.parse(localStorage.getItem('user'));
-            let list = []
-            if (!user) history.push('/login');
-            list = await fetchProductList();
-            setProductList(list);
-        }
-        didMount();
-    }, [history])
+  async function didMount() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    console.log(user);
+    let list = [];
+    if (!user) history.push('/login');
+    list = await fetchProductList();
+    setProductList(list);
+  }
 
-    const generateTotal = () => {
-
-        const total = productList.reduce((acc, item) => {
-            let sum = 0;
-            if (item.quantity) {
-                sum = parseFloat(item.quantity) * parseFloat(item.price)
-                return acc + sum
-            }
-            return acc;
-        }, 0)
-        setValorTotal(total.toFixed(2));
-        return total.toFixed(2);
+  useEffect(() => {
+    const savedList = localStorage.getItem('productList');
+    if (savedList) {
+      setProductList(JSON.parse(savedList));
+    } else {
+      didMount();
     }
+  }, [history]);
 
-    const generateProducts = () => {
-        return (<div>
-            {productList.length === zero
-                ? (<h3>Carregando...</h3>)
-                : productList.map((item, index) => (
-                    <div
-                        key={index}
-                    >
-                        <img
-                            data-testid={`${index}-product-img`}
-                            src={item.url_image}
-                            alt="bebida da lista"
-                            width="50px"
-                        />
-                        <p
-                            data-testid={`${index}-product-name`}
-                        >
-                            {item.name}
-                        </p>
-                        <p
-                        data-testid="0-product-price"
-                        >
-                            R${item.price}
-                        </p>
-                        <button
-                            type="button"
-                            data-testid={`${index}-product-plus`}
-                            onClick={() => {
-                                const list = [...productList]
-                                list[index].quantity = list[index].quantity ?
-                                    list[index].quantity + 1 : 1;
-                                setProductList(list)
-                                localStorage.setItem('productList', productList);
-                                generateTotal()
-                            }}
-                        >
-                            &uArr;
-              </button>
-                        <button
-                            type="button"
-                            data-testid={`${index}-product-minus`}
+  const generateTotal = () => {
+    const total = productList.reduce((acc, item) => {
+      let sum = 0;
+      if (item.quantity) {
+        sum = parseFloat(item.quantity) * parseFloat(item.price);
+        return acc + sum;
+      }
+      return acc;
+    }, 0);
+    setValorTotal(total.toFixed(2).toString().replace('.', ','));
+    // return total.toFixed(2);
+  };
 
-                            onClick={() => {
-                                const list = [...productList]
-                                list[index].quantity = list[index].quantity && list[index].quantity > 0 ?
-                                    list[index].quantity - 1 : 0;
-                                setProductList(list)
-                                localStorage.setItem('productList', productList);
-                                generateTotal();
-                            }}
-                        >
+  useEffect(() => {
+    generateTotal();
+  }, [generateTotal, productList]);
 
-                            &dArr;
-              </button>
-                        <h3
-                            data-testid="0-product-qtd"
-                        >
-                            {productList[index].quantity || 0}
-                        </h3>
-                    </div>
-                ))}
-    )
-            <button
-                data-testid="checkout-bottom-btn"
-                onClick={() => history.push('/checkout')}
-                disabled={valorTotal === 0}
-
-            >
-                Ver Carrinho:
+  const generateProducts = () => (
+    <div>
+      {productList.length === zero
+        ? (<h3>Carregando...</h3>)
+        : productList.map((item, index) => (
+          <div
+            key={ index }
+          >
+            <img
+              data-testid={ `${index}-product-img` }
+              src={ item.url_image }
+              alt="bebida da lista"
+              width="50px"
+            />
             <p
-                    data-testid="checkout-bottom-btn-value"
-                >
-                    R${valorTotal}
-                </p>
+              data-testid={ `${index}-product-name` }
+            >
+              {item.name}
+            </p>
+            <p
+              data-testid={ `${index}-product-price` }
+            >
+              R$
+              {' '}
+              {item.price.replace('.', ',')}
+            </p>
+            <button
+              type="button"
+              data-testid={ `${index}-product-plus` }
+              onClick={ () => {
+                const list = [...productList];
+                list[index].quantity = list[index].quantity
+                  ? list[index].quantity + 1 : 1;
+                setProductList(list);
+                localStorage.setItem('productList', JSON.stringify(productList));
+              // generateTotal()
+              } }
+            >
+              &uArr;
             </button>
-        </div>)
-    }
+            <button
+              type="button"
+              data-testid={ `${index}-product-minus` }
 
-    return (
-        generateProducts()
-    );
+              onClick={ () => {
+                const list = [...productList];
+                list[index].quantity = list[index].quantity && list[index].quantity > 0
+                  ? list[index].quantity - 1 : 0;
+                setProductList(list);
+                localStorage.setItem('productList', JSON.stringify(productList));
+              // generateTotal();
+              } }
+            >
+
+              &dArr;
+            </button>
+            <h3
+              data-testid={ `${index}-product-qtd` }
+            >
+              {productList[index].quantity || 0}
+            </h3>
+          </div>
+        ))}
+
+      <button
+        data-testid="checkout-bottom-btn"
+        type="button"
+        onClick={ () => history.push('/checkout') }
+        disabled={ valorTotal === '0,00' }
+
+      >
+        Ver Carrinho:
+        <p
+          data-testid="checkout-bottom-btn-value"
+        >
+          R$
+          {' '}
+          {valorTotal}
+        </p>
+      </button>
+    </div>
+  );
+
+  return (
+    generateProducts()
+  );
 }
 
 export default Products;
--- front-end/src/pages/ProductsPage.js
+++ front-end/src/pages/ProductsPage.js
@@ -3,12 +3,12 @@ import Products from '../components/Products';
 import MenuTop from '../components/MenuTop';
 
 function ProductsPage() {
-    return (
-        <div className="form-page">
-            <MenuTop title="Products" />
-            <Products />
-        </div>
-    );
+  return (
+    <div className="form-page">
+      <MenuTop />
+      <Products />
+    </div>
+  );
 }
 
 export default ProductsPage;
