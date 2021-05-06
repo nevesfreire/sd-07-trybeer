167cc53daa37630c90e8bdf413df27c9edfe434f
Eriquy Jhordan <eriquy@gmail.com>
2021-05-06 16:14:07 -0300

    servindo imagens

--- back-end/controller/ProductController.js
+++ back-end/controller/ProductController.js
@@ -1,11 +1,12 @@
 const url = require('url');
 const ProductService = require('../service/ProductService');
 
-
 const getAllProducts = async (req, res) => {
   try {
     const result = await ProductService.getAll();
-    const allProducts = result[0].map((product) => ({...product, url_image: url.parse(product.url_image).href}));
+    const allProducts = result[0].map((product) => (
+      // eslint-disable-next-line camelcase
+      { ...product, url_image: url.parse(product.url_image).href }));
     return res.status(200).json(allProducts);
   } catch (error) {
     return res.status(500).json({ error: error.message });
