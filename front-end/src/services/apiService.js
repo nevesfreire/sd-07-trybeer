85afcc92353174fee0299520b2ae40a4e8db2208
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-05-07 11:49:21 -0300

    Linter 💩

--- front-end/src/services/apiService.js
+++ front-end/src/services/apiService.js
@@ -1,10 +1,11 @@
-const userLogin = async ({ email, password }) =>
-    fetch('http://localhost:3001/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    }).then((response) => response.json());
+const userLogin = ({ email, password }) => fetch('http://localhost:3001/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ email, password }),
+})
+  .then((response) => response.json())
+  .catch((error) => console.log(error));
 
 export default userLogin;
