ae9be8ef23a3a790119da76d57af1458a093696b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-05-07 09:55:48 -0300

    Adicionando ;

--- front-end/src/components/LoginForm/index.jsx
+++ front-end/src/components/LoginForm/index.jsx
@@ -28,8 +28,8 @@ function LoginForm() {
     // req da api enviando:
     return userLogin(user).then((apiResponse) => {
       localStorage.setItem('user', JSON.stringify(apiResponse));
-      console.log(`Console log do apiResponse: ${apiResponse}`)
-      console.log(`Console log do role: ${apiResponse.role}`)
+      console.log(`Console log do apiResponse: ${apiResponse}`);
+      console.log(`Console log do role: ${apiResponse.role}`);
       if (apiResponse.role === 'administrator') {
         return history.push('/admin/orders');
       }
