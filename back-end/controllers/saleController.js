2c788e2450dd292ec746dc7dccb22459a26e37f8
rodrigoderkian <derkianpromo@gmail.com>
2021-05-07 15:59:41 -0300

    Finalizando o requisito 6

--- back-end/controllers/saleController.js
+++ back-end/controllers/saleController.js
@@ -7,11 +7,13 @@ const createSale = async (request, response) => {
     const { email, total_price, delivery_address, delivery_number, products_sales } = request.body;
     // const { authorization } = request.headers;
     const result = await saleServices.createSale(email, total_price, delivery_address, delivery_number, products_sales);
-    response.status(STATUS_CODE.SUCCESS).json(result);
+    response.status(STATUS_CODE.SUCCESS).json({ message: result });
   } catch (error) {
     response.status(STATUS_CODE.BAD_REQUEST).json({ message: error.message });
   }
 };
+
+
 
 module.exports = {
   createSale,
