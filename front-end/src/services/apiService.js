367a12c95025862a8b06cc87e28fd74d34cca8cb
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-05-07 11:44:41 -0300

    Modificando a chamada da api para utilizar then

--- front-end/src/services/apiService.js
+++ front-end/src/services/apiService.js
@@ -1,17 +1,10 @@
-const userLogin = async ({ email, password }) => {
-  try {
-    const request = await fetch('http://localhost:3001/login', {
+const userLogin = async ({ email, password }) =>
+    fetch('http://localhost:3001/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ email, password }),
-    });
-    const response = await request.json();
-    return response;
-  } catch (err) {
-    console.error(err);
-  }
-};
+    }).then((response) => response.json());
 
 export default userLogin;
