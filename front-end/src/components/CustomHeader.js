a3bae0374a4af43ed7ef25a0a5e37a904b98e080
hvenses <hvenses@gmail.com>
2021-05-07 16:06:39 -0300

    Commitando estrutura back req5

--- front-end/src/components/CustomHeader.js
+++ front-end/src/components/CustomHeader.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Header } from 'semantic-ui-react';
-
-
 import CustomTopMenu from './CustomTopMenu';
 
 const CustomHeader = ({ message }) => (
