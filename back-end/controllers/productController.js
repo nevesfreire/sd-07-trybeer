1a99912178a3731ffe8c7b893175dd08219bb568
Nonato <lucas.nonato1@hotmail.com>
2021-05-09 21:12:11 -0300

    fix: ImageRequest

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -15,7 +15,8 @@ const getAll = async (req, res) => {
 const getImageProduct = async (req, res) => {
   try {
     const imageProduct = await productService.getImageProduct(req.params.name);
-    return res.status(OK).sendFile(imageProduct, { root: process.cwd() });
+    const { http, message } = imageProduct;
+    return res.status(http).sendFile(message, { root: process.cwd() });
   } catch (error) {
     console.log(error);
     return res.status(INTERNAL_SERVER_ERROR).json(error);
