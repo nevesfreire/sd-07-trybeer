d40f0b2b6600bce0f15196a6d94b7d928d5b65ff
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-05-10 18:11:51 -0300

    reparando a chamada para a API

--- front-end/src/components/ClientDetailOrder.jsx
+++ front-end/src/components/ClientDetailOrder.jsx
@@ -8,24 +8,48 @@ const ClientDetailOrder = () => {
 
   const history = useHistory();
   const path = history.location.pathname;
-  const idPathName = path.split('/');
-  // console.log('parametro', idPathName[2]);
+  const idPathName = path.split('/')[2];
+  console.log('parametro', idPathName);
+  const data = [
+    {
+      delivery_number: '0d4291cb-11bf-4421-9214-e6cea57a03d2',
+      date: '10/05',
+      quantity: '5',
+      name: 'Skol Lata 250ml',
+      price: '2.20',
+      total_price: '48.50',
+    },
+    {
+      delivery_number: '0d4291cb-11bf-4421-9214-e6cea57a03d2',
+      date: '10/05',
+      quantity: '5',
+      name: 'Heineken 600ml',
+      price: '7.50',
+      total_price: '48.50',
+    },
+  ];
 
-  useEffect(() => {
-    const getClientOrders = async () => {
-      const result = await getDetailOrders(idPathName[2]);
-      setOrder(result);
-      setLoading(false);
-    };
-    getClientOrders();
-    // getDetailOrdersidPathNamethen((r) => { setOrder(r); });
-  }, [idPathName]);
+  // useEffect(() => {
+  //   const getClientOrders = async () => {
+  //     const result = await getDetailOrders(idPathName);
+  //     setOrder(result);
+  //     setLoading(false);
+  //   };
+  //   getClientOrders();
+  //   // getDetailOrdersidPathNamethen((r) => { setOrder(r); });
+  // }, [idPathName]);
 
   return (
     <div>
       <h1 data-testid="top-title">Detalhes de Pedido</h1>
       <div>
-        {!loading ? console.log(order) : 'carregando'}
+        {data.map(({ name, key, price }) => (
+          <div key={ key }>
+            <h5 key={ key }>{name}</h5>
+            <p>{price}</p>
+          </div>
+        ))}
+        {/* {!loading ? console.log(order) : 'carregando'} */}
       </div>
     </div>
   );
