0f1b440079a42d117df987544577e07b99b2c964
ilanribeiro <ilanribeiro@gmail.com>
2021-05-10 20:25:25 -0300

    Backend: Adiciona requisito 6 - realizados testes insomnia

---
+++ back-end/controllers/checkoutController.js
@@ -0,0 +1,55 @@
+const checkoutService = require('../services/checkoutService');
+
+const CREATE = 201;
+const OK = 200;
+const ERROR = 400;
+const CONFLICT = 409;
+
+const getAllSales = async (request, response) => {
+  try {
+      const sale = await checkoutService.getAllSales();
+    return response.status(OK).json(sale);
+  } catch (error) {
+    console.error(error);
+    response.status(ERROR).json({ message: error.message });
+  }
+};
+
+const getSaleById = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const sale = await saleService.getSaleById(id);
+    if (!sale) {
+      const ERR_MESSAGE = 'sale not found';
+      throw new Error(ERR_MESSAGE);
+    }
+    return response.status(OK).json(sale);
+  } catch (error) {
+    const { message } = error;
+    return response.status(ERROR).json({ message });
+  }
+};
+
+const createSale = async (request, response) => {
+  try {
+    /*const {
+      user_id,
+      total_price,
+      delivery_address,
+      delivery_number,
+      cart
+    } = request.body;*/
+    const objParams = {...request.body}
+    const sale  = await checkoutService.createSale(objParams);
+    return response.status(CREATE).json(sale);
+  } catch (error) {
+    console.error(error);
+    const { message } = error;
+    if (message.includes('registered')) {
+      return response.status(CONFLICT).json({ message });
+    }
+    response.status(ERROR).json({ message: error.message });
+  }
+}
+
+module.exports = { getAllSales, getSaleById, createSale }
---
+++ back-end/services/checkoutService.js
@@ -0,0 +1,18 @@
+const saleModel = require('../models/saleModel');
+
+const getAllSales = async () => {
+  const allSales = await saleModel.getAllSales();
+  return allSales;
+};
+
+const getSaleById = async (id) => {
+  const sale = await saleModel.getSaleById(id);
+  return sale;
+};
+
+const createSale = async (ObjParams) =>{
+  const sale  = await saleModel.createCheckout(ObjParams);
+  return sale;
+};
+
+module.exports = { getAllSales, getSaleById, createSale};
