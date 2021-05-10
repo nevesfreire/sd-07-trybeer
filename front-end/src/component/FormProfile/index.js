41701bf995e904820cdf7f9d016019c604200aee
Renato Silva <renatosdev@gmail.com>
2021-05-10 15:26:19 -0300

    requisito4-perfil

---
+++ front-end/src/component/FormProfile/index.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { getToLocalStorage } from '../../utils/localStorage'
+
+const FormDefault = {
+  name:'',
+  email:'',
+}
+function FormProfile() {
+  
+  const [ formProfile, setFormProfile ] = useState(FormDefault);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormProfile({...formProfile, [name]: value})
+  };
+  
+  useEffect(() => {
+    const email = getToLocalStorage().email
+    console.log(email)
+  },[])
+  
+  
+  return (
+    <>
+      <label>
+        Nome
+        <input type="text" 
+        name="name"
+        data-testid="profile-name-input"
+        value={formProfile.name}
+        onChange={(event) => handleInputChange(event)}
+        />
+      </label>
+      <label>
+        Email
+        <input type="email" 
+        readOnly="readOnly"
+        name="email"
+        data-testid="profile-email-input"
+        value={formProfile.email}
+        onChange={(event) => handleInputChange(event)}
+        />
+      </label>
+      <button
+      type="submit"
+      data-testid="profile-save-btn"
+      >
+        Salvar
+      </button>
+    </>  
+  )
+}
+
+export default FormProfile;
---
+++ front-end/src/pages/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormProfile from '../../component/FormProfile'
+
+export default function Profile() {
+  return (
+  <>
+  <FormProfile/>
+  </>
+  )
+}
+
