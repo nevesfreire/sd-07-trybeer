7831d4d98dbea612dd8e81be8ae60c9f6e6bdd40
Éric Vinícius <ericcobain3@gmail.com>
2021-05-10 11:43:54 -0300

    adicionei a tag h1 com o titulo, em orders.js

--- front-end/src/pages/user/Orders.js
+++ front-end/src/pages/user/Orders.js
@@ -7,7 +7,7 @@ import OrderCard from '../../components/OrderCard';
 
 function Orders() {
   const [orders, setOrders] = useState([]);
-  const { sideIsActive, setPageTitle } = useContext(MyContext);
+  const { sideIsActive } = useContext(MyContext);
   const history = useHistory();
   useEffect(() => {
     const getUser = () => {
@@ -17,9 +17,9 @@ function Orders() {
     getUser();
   }, [history]);
 
-  useEffect(() => {
-    setPageTitle('Meu pedidos');
-  }, [setPageTitle]);
+  // useEffect(() => {
+  //   setPageTitle('Meu pedidos');
+  // }, [setPageTitle]);
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -36,6 +36,7 @@ function Orders() {
   }, []);
   return (
     <div>
+      <h1 data-testid="top-title">Meus Pedidos</h1>
       <MenuTopMobile />
       { sideIsActive && <SideBarMobile /> }
       {orders.map((order, index) => (<OrderCard
