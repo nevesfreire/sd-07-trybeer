942b1e736d65086a8804cdee466285351492bfa3
Zeca Costa <jccostaso@gmail.com>
2021-05-11 13:57:21 -0300

    first commit

--- back-end/controllers/login.js
+++ back-end/controllers/login.js
@@ -5,18 +5,21 @@ const httpStatus = require('./httpStatus');
 const secret = 'meusegredoparajwt';
 
 const userLogin = async (req, res) => {
+  console.log('entrei no controller');
+  
   try {
     const jwtConfig = {
       expiresIn: '7d',
       algorithm: 'HS256',
     };
     const { email, password } = req.body;
+    console.log(req.body)
     const token = jwt.sign({ data: email }, secret, jwtConfig);
     const login = await loginService(email, password);
-    console.log(login);
     const loginInfo = {
       name: login.name, role: login.role, email: login.email,
     };
+    console.log(loginInfo)
     res.status(httpStatus.OK).json({ ...loginInfo, token });
   } catch (error) {
     res.status(httpStatus.BAD_REQUEST).json({
--- back-end/models/login.js
+++ back-end/models/login.js
@@ -1,11 +1,12 @@
 const connection = require('../configuration/configuration');
 
 const loginModel = async (email) => {
+  console.log('entrei no model');
+  console.log(email)
   const [login] = await connection.execute(
     'SELECT * FROM users WHERE email = ?',
     [email],
   );
-  
   return login[0];
 };
 
