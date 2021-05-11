90e165ad11fd207a2de8aaa778771cb8a5af03ef
moacyrrln <moacyrrln@gmail.com>
2021-05-11 17:13:36 -0300

    falha no login

--- front-end/src/Pages/client/Products.js
+++ front-end/src/Pages/client/Products.js
@@ -7,7 +7,12 @@ function Products() {
   const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState("");
-  const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(999);
+  const [logado, setLogado] = useState(false);
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (user) setLogado(true);
 
   useEffect(() => {
     setIsLoading(true);
@@ -17,8 +22,9 @@ function Products() {
         setProducts(products);
 
         const initialCart = () => {
-          if (localStorage.getItem("cart") !== null)
+          if (localStorage.getItem("cart") !== null) {
             return JSON.parse(localStorage.getItem("cart"));
+          }
 
           let array = [];
           for (let id = 1; id <= products.length; id++) {
@@ -29,18 +35,23 @@ function Products() {
 
         setCart(initialCart());
 
-        
+        console.log(logado);
 
         setIsLoading(false);
       });
   }, []);
 
+  useEffect(() => {
+    let sum = 0;
 
+    products.map((product) => {
+      sum += parseFloat(product.price) * getQtd(product.id);
+    });
 
+    setTotal(sum);
+  }, [cart]);
 
   const history = useHistory();
-
-
 
   function addQuantity(id) {
     let add = cart.find((product) => product.id === id);
@@ -51,11 +62,10 @@ function Products() {
     let sum = 0;
 
     products.map((product, index) => {
-      sum += (parseFloat(product.price) * getQtd(product.id))
-    })
+      sum += parseFloat(product.price) * getQtd(product.id);
+    });
 
     setTotal(sum);
-
   }
 
   function subQuantity(id) {
@@ -67,8 +77,8 @@ function Products() {
     let sum = 0;
 
     products.map((product, index) => {
-      sum += (parseFloat(product.price) * getQtd(product.id))
-    })
+      sum += parseFloat(product.price) * getQtd(product.id);
+    });
 
     setTotal(sum);
   }
@@ -78,66 +88,82 @@ function Products() {
     return add.qtd;
   }
 
+  // console.log(JSON.parse(localStorage.getItem('user')) === '')
+  // console.log(JSON.parse(localStorage.getItem('user')) !== '')
 
+  // const xablau = JSON.parse(localStorage.getItem('user'))
+
+  // let vazio
+
+  // if (xablau === '') vazio = true
+  // else vazio = false
 
   return (
     <div>
-      <Header />
-      <h1 data-testid="top-title">Products</h1>
-      <hr />
-      {isLoading ? (
-        <p>carregando</p>
+      {logado ? (
+        <Redirect to="login" />
       ) : (
-        products.map((e, index) => (
-          <div className="App" key={e.id}>
-            <img
-              data-testid={`${index}-product-img`}
-              src="Becks.jpg"
-            />
-            <div>{e.url_image}</div>
-            <div data-testid={`${index}-product-name`}>{e.name}</div>
-            <div data-testid={`${index}-product-price`}>
-              R$ {e.price.replaceAll(".", ",")}
-            </div>
-            <button
-              data-testid={`${index}-product-plus`}
-              name={"plus ".concat(e.id)}
-              onClick={() => {
-                addQuantity(e.id);
-              }}
-            >
-              +
-            </button>
-            <input
-              data-testid={`${index}-product-qtd`}
-              type="number"
-              name={"quantity".concat(e.id)}
-              value={getQtd(e.id)}
-            />
-
-            <button
-              data-testid={`${index}-product-minus`}
-              name={"plus ".concat(e.id)}
-              onClick={() => {
-                subQuantity(e.id);
-              }}
-            >
-              -
-            </button>
-            <hr />
-          </div>
-        ))
-      )}
-      <button
-        data-testid="checkout-bottom-btn"
-        onClick={() => {
-          history.push("/checkout");
-        }}
-      >
         <div>
-          Ver carrinho R$:{" "}<div data-testid="checkout-bottom-btn-value">{ parseFloat(total).toFixed(2) }</div>
+          <Header />
+          <h1 data-testid="top-title">Products</h1>
+          <hr />
+          {isLoading ? (
+            <p>carregando</p>
+          ) : (
+            products.map((e, index) => (
+              <div className="App" key={e.id}>
+                <img data-testid={`${index}-product-img`} src="Becks.jpg" />
+                <div>{e.url_image}</div>
+                <div data-testid={`${index}-product-name`}>{e.name}</div>
+                <div data-testid={`${index}-product-price`}>
+                  R$ {e.price.replaceAll(".", ",")}
+                </div>
+                <button
+                  data-testid={`${index}-product-plus`}
+                  name={"plus ".concat(e.id)}
+                  onClick={() => {
+                    addQuantity(e.id);
+                  }}
+                >
+                  +
+                </button>
+                <span
+                  data-testid={`${index}-product-qtd`}
+                  type="number"
+                  name={"quantity".concat(e.id)}
+                >
+                  {getQtd(e.id)}
+                </span>
+
+                <button
+                  data-testid={`${index}-product-minus`}
+                  name={"plus ".concat(e.id)}
+                  onClick={() => {
+                    subQuantity(e.id);
+                  }}
+                >
+                  -
+                </button>
+                <hr />
+              </div>
+            ))
+          )}
+          <button
+            disabled={total === 0}
+            data-testid="checkout-bottom-btn"
+            onClick={() => {
+              history.push("/checkout");
+            }}
+          >
+            <div>
+              Ver Carrinho
+              <div data-testid="checkout-bottom-btn-value">
+                R$ {parseFloat(total).toFixed(2).replaceAll(".", ",")}
+              </div>
+            </div>
+          </button>
         </div>
-      </button>
+      )}
     </div>
   );
 }
