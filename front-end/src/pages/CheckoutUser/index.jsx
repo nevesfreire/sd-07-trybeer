22000a967368a63d2807fd2b0e3e534860ed5df0
dennerpl <denner.souzapl@gmail.com>
2021-05-11 20:04:26 -0300

    Agora falta ajustar até passar nos testes

--- front-end/src/pages/CheckoutUser/index.jsx
+++ front-end/src/pages/CheckoutUser/index.jsx
@@ -1,18 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { fetchFinishSale } from '../../services';
 
 export default function CheckoutUser() {
   const [cartItems, setCartItems] = useState([]);
+  const [emptyCart, setEmptyCart] = useState(true);
   const [address, setAddress] = useState('');
   const [houseNumber, setHouseNumber] = useState('');
   const [totalCart, setTotalCart] = useState('');
+  const [alertController, setAlertController] = useState(false);
+
   useEffect(() => {
     const localStorageCart = JSON.parse(localStorage.getItem('cart'));
     const localStorageTotalCart = JSON.parse(localStorage.getItem('totalCart'));
     setCartItems(localStorageCart);
     setTotalCart(localStorageTotalCart);
   }, []);
+
+  useEffect(() => {
+    if (cartItems.length === 0) {
+      setEmptyCart(true);
+    } else {
+      setEmptyCart(false);
+    }
+  }, [cartItems]);
+
+  const timeoutMessage = 5000;
+
+  // const handleUpdateMessage = async () => {
+  //   setAlertController(true);
+  //   setTimeout(() => {
+  //     setAlertController(false);
+  //   }, timeoutMessage);
+  // };
 
   const removeItemFromCart = (id) => {
     const atualItem = cartItems.find((product) => product.id === id);
@@ -23,6 +44,8 @@ export default function CheckoutUser() {
     setTotalCart(totalCart - atualItem.totalPrice);
   };
 
+  const history = useHistory();
+
   const finishButton = async (event) => {
     event.preventDefault();
     const { id } = JSON.parse(localStorage.getItem('user'));
@@ -31,25 +54,36 @@ export default function CheckoutUser() {
       houseNumber,
     };
     await fetchFinishSale(id, totalCart, addressObject, cartItems);
+    setAlertController(true);
+    setTimeout(() => {
+      localStorage.setItem('totalCart', '0,00');
+      localStorage.setItem('cart', JSON.stringify([]));
+      setAlertController(false);
+      history.push('/products');
+    }, timeoutMessage);
   };
   return (
     <div>
       <ul>
-        {cartItems.map((product, index) => (
-          <li key={ index }>
-            <div data-testid={ `${index}-product-qtd-input` }>{product.quantity}</div>
-            <div data-testid={ `${index}-product-name` }>{product.name}</div>
-            <div data-testid={ `${index}-product-total-value` }>{product.totalPrice}</div>
-            <div data-testid={ `${index}-product-unit-price` }>{product.price}</div>
-            <button
-              type="button"
-              data-testid={ `${index}-product-removal-button` }
-              onClick={ () => removeItemFromCart(product.id) }
-            >
-              X
-            </button>
-          </li>
-        ))}
+        Produtos
+        {emptyCart ? <h2>Não há produtos no carrinho</h2>
+          : cartItems.map((product, index) => (
+            <li key={ index }>
+              <div data-testid={ `${index}-product-qtd-input` }>{product.quantity}</div>
+              <div data-testid={ `${index}-product-name` }>{product.name}</div>
+              <div data-testid={ `${index}-product-total-value` }>
+                {product.totalPrice}
+              </div>
+              <div data-testid={ `${index}-product-unit-price` }>{product.price}</div>
+              <button
+                type="button"
+                data-testid={ `${index}-product-removal-button` }
+                onClick={ () => removeItemFromCart(product.id) }
+              >
+                X
+              </button>
+            </li>
+          ))}
       </ul>
       <div data-testid="order-total-value">{ totalCart }</div>
       <Form className="form__login">
@@ -81,6 +115,7 @@ export default function CheckoutUser() {
         >
           Finalizar Pedido
         </Button>
+        { alertController && <p>Compra realizada com sucesso</p> }
       </Form>
     </div>
   );
