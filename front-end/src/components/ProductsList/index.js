5a748299914c03fc8385ff493577d2972b8a933b
moraisgabri <gabriel8azevedo@gmail.com>
2021-05-10 22:01:15 -0300

    requisito 5, a esperança e a ultima que morre

--- front-end/src/components/ProductsList/index.js
+++ front-end/src/components/ProductsList/index.js
@@ -4,36 +4,56 @@ import { getProducts } from '../../services/apiService';
 
 export default function ProductsList() {
   const [products, setProducts] = useState(null);
-  const [productQtt, setProductQtt] = useState(0);
-  const [totalValue, setTotalValue] = useState('00,00');
+  const [totalValue, setTotalValue] = useState(0);
   const history = useHistory();
 
   useEffect(() => {
     const fetchProducts = async () => {
       const currentUser = JSON.parse(localStorage.getItem('user'));
-
+      if(!currentUser) return null
       const response = await getProducts(currentUser.token)
         .then((apiResponse) => apiResponse);
 
-      setProducts(response.map((item) => ({ ...item, productQtt })));
+      setProducts(response.map((item) => ({ ...item, productQtt: 0 })));
       setTotalValue(0);
     };
 
     fetchProducts();
   }, []);
 
-  const addProdQtt = (e, id) => {
-    const { name } = e.target;
-    if (Number(name) === id) {
-      const addProdValue = productQtt + 1;
-      setProductQtt(addProdValue);
-    }
+  const addProdQtt = (id) => {
+    const searchedProduct = products.find((e) => e.id === id )
+    const index = products.indexOf(searchedProduct);
+    const newArr = [...products]
+    newArr[index].productQtt += 1;
+    const price = parseFloat(searchedProduct.price)
+    setProducts(newArr)
+    setTotalValue(totalValue + price)
+    console.log({ products })
   };
+
+  const decProdQtt = (id) => {
+    const searchedProduct = products.find((e) => e.id === id )
+    const index = products.indexOf(searchedProduct);
+    if(searchedProduct.productQtt === 0) {
+      return null
+    }
+    const newArr = [...products]
+    newArr[index].productQtt -= 1;
+    const price = parseFloat(searchedProduct.price)
+    setTotalValue(totalValue - price)
+    setProducts(newArr)
+    console.log({ products })
+  };
+
+  console.log('renderizou de novo')
 
   return (
     <div style={ { display: 'flex', flexDirection: 'column', alignItems: 'center' } }>
       { !products ? <span>Loading...</span>
-        : products.map((item, index) => (
+        : products.map((item, index) => {
+          console.log({qty: item.productQtt})
+          return(
           <div key={ item.id }>
             <img
               style={ { width: '100px' } }
@@ -50,7 +70,7 @@ export default function ProductsList() {
               type="button"
               name={ item.id }
               data-testid={ `${index}-product-plus` }
-              onClick={ (e) => addProdQtt(e, item.id) }
+              onClick={ () => addProdQtt(item.id) }
             >
               +
             </button>
@@ -58,15 +78,16 @@ export default function ProductsList() {
             <button
               type="button"
               data-testid={ `${index}-product-minus` }
+              onClick={ () => decProdQtt(item.id) }
             >
               -
             </button>
           </div>
-        ))}
+        )})}
       <footer
         style={ { background: 'gray', bottom: '0', position: 'fixed', padding: '10px' } }
       >
-        <span data-testid="checkout-bottom-btn-value">{`R$ ${totalValue}`}</span>
+        <span data-testid="checkout-bottom-btn-value">{`R$ ${totalValue.toFixed(2).replace('.', ',')}`}</span>
         <button
           type="button"
           data-testid="checkout-bottom-btn"
