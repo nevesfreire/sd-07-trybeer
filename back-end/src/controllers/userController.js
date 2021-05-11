fc98166c9fb2ce45801bd92a2fe016bb3be0cfc6
Luiz Mariz <lfsmariz@gmail.com>
2021-05-11 14:40:22 -0300

    endpoint for update

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -2,8 +2,8 @@ const userService = require("../services/user");
 
 const updateUser = async (req, res) => {
   const { name, old } = req.body;
-  const update = await userService(name, old);
-  res.status(200).json({ update });
+  const message= await userService.updateUser(name, old);
+  res.status(200).json({ message });
 };
 
 module.exports = {
--- back-end/src/models/User.js
+++ back-end/src/models/User.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 
 const updateUser = async (name, oldName) => {
   const [user] = await connection.execute(
-    `UPDATE Trybeer.users SET users.name = '${name}'
-    WHERE name = '${oldName}';`,
+    `UPDATE Trybeer.users SET users.name = '${name}' WHERE name = '${oldName}'`,
   );
+  console.log(user) 
   return 'update Sucess';
 };
+
 
 module.exports = {
   updateUser
