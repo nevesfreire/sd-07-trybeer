c77807cdda7a594d04cbabf4a3c39c26b51f15dd
moraisgabri <gabriel8azevedo@gmail.com>
2021-05-10 22:41:53 -0300

    sera se vai agora?

--- front-end/src/components/ProductsList/index.js
+++ front-end/src/components/ProductsList/index.js
@@ -3,9 +3,16 @@ import { useHistory } from 'react-router-dom';
 import { getProducts } from '../../services/apiService';
 
 export default function ProductsList() {
+  const totalVal = JSON.parse(localStorage.getItem('total'));
+  const [totalValue, setTotalValue] = useState(totalVal || 0);
   const [products, setProducts] = useState(null);
-  const [totalValue, setTotalValue] = useState(0);
   const history = useHistory();
+
+  console.log(totalValue);
+
+  // useEffect(() => {
+  //
+  // }, [products])
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -15,7 +22,6 @@ export default function ProductsList() {
         .then((apiResponse) => apiResponse);
 
       setProducts(response.map((item) => ({ ...item, productQtt: 0 })));
-      setTotalValue(0);
     };
 
     fetchProducts();
@@ -28,8 +34,8 @@ export default function ProductsList() {
     newArr[index].productQtt += 1;
     const price = parseFloat(searchedProduct.price);
     setProducts(newArr);
+    localStorage.setItem('total', totalValue + price);
     setTotalValue(totalValue + price);
-    console.log({ products });
   };
 
   const decProdQtt = (id) => {
@@ -41,60 +47,58 @@ export default function ProductsList() {
     const newArr = [...products];
     newArr[index].productQtt -= 1;
     const price = parseFloat(searchedProduct.price);
+    localStorage.setItem('total', totalValue + price);
     setTotalValue(totalValue - price);
     setProducts(newArr);
-    console.log({ products });
   };
 
   console.log('renderizou de novo');
 
   return (
     <div style={ { display: 'flex', flexDirection: 'column', alignItems: 'center' } }>
-      { !products ? <span>Loading...</span>
-        : products.map((item, index) => {
-          console.log({ qty: item.productQtt });
-          return (
-            <div key={ item.id }>
-              <img
-                style={ { width: '100px' } }
-                src={ item.url_image }
-                alt={ item.name }
-                data-testid={ `${index}-product-img` }
-              />
-              <p data-testid={ `${index}-product-name` }>{item.name}</p>
-              <p data-testid={ `${index}-product-price` }>
-                R$
-                {item.price.replace('.', ',')}
-              </p>
-              <button
-                type="button"
-                name={ item.id }
-                data-testid={ `${index}-product-plus` }
-                onClick={ () => addProdQtt(item.id) }
-              >
-                +
-              </button>
-              <span data-testid={ `${index}-product-qtd` }>{item.productQtt}</span>
-              <button
-                type="button"
-                data-testid={ `${index}-product-minus` }
-                onClick={ () => decProdQtt(item.id) }
-              >
-                -
-              </button>
-            </div>
-          );
-        })}
+      { !products ? <p>Loading...</p>
+        : products.map((item, index) => (
+          <div key={ item.id }>
+            <img
+              style={ { width: '100px' } }
+              src={ item.url_image }
+              alt={ item.name }
+              data-testid={ `${index}-product-img` }
+            />
+            <p data-testid={ `${index}-product-name` }>{item.name}</p>
+            <p data-testid={ `${index}-product-price` }>
+              R$
+              {item.price.replace('.', ',')}
+            </p>
+            <button
+              type="button"
+              name={ item.id }
+              data-testid={ `${index}-product-plus` }
+              onClick={ () => addProdQtt(item.id) }
+            >
+              +
+            </button>
+            <p data-testid={ `${index}-product-qtd` }>{item.productQtt}</p>
+            <button
+              type="button"
+              data-testid={ `${index}-product-minus` }
+              onClick={ () => decProdQtt(item.id) }
+            >
+              -
+            </button>
+          </div>
+        ))}
       <footer
         style={ { background: 'gray', bottom: '0', position: 'fixed', padding: '10px' } }
       >
-        <span data-testid="checkout-bottom-btn-value">
+        <p data-testid="checkout-bottom-btn-value">
           {`R$ ${totalValue.toFixed(2).replace('.', ',')}`}
-        </span>
+        </p>
         <button
           type="button"
           data-testid="checkout-bottom-btn"
           onClick={ () => history.push('/checkout') }
+          disabled={ totalValue === 0 }
         >
           Ver Carrinho
         </button>
