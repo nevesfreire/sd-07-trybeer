7deeee19a17f8100e5334de99f51ecd605fe3446
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-05-11 15:29:51 -0300

    Tentativa de usar o SuperTest

--- back-end/test/auth.test.js
+++ back-end/test/auth.test.js
@@ -1,12 +1,34 @@
+let request = require('supertest');
 const messages = require('../helpers/dictonary');
-const { auth: { service } } = require('../resources');
+const { auth: { service, controller } } = require('../resources');
 
-describe('AUTH SERVICE TEST', async () => {
+describe('AUTH TEST', () => {
+
+  request = request('http://localhost:3001');
 
   const users = [
     { name: 'Tryber Admin', email: 'tryber@trybe.com.br', password: '123456', role: 'administrator' },
     { name: 'testuser', email: 'user@test.com', password: 'test123', role: 'client'}
   ];
+
+  // TENTATIVA DE USAR O SUPER TEST - NÃO FINALIZADO
+  it('Testa o Controller', async (done) => {
+    request.post('/login')
+      .set('Accept', 'application/json')
+      .field('email', users[0].email)
+      .field('password', users[0].password)
+      .expect(200)
+      .then(response => {
+        assert(response.body.name, users[0].name);
+        assert(response.body.email, users[0].email);
+        assert(response.body.role, users[0].role).toBe();
+        assert(response.body.token).not.toBeNull();
+        done();
+      })
+      .catch(err => done(err))
+  });
+
+  
 
   it('Será validado que o campo "email" é obrigatório', async () => {
     await service.login(undefined, users[0].password)
