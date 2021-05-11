a9b316b4b2a17ddc3764dac754c0dfcb54f40feb
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-05-11 19:11:33 -0300

    Req 5 quase...

--- front-end/src/store/provider/index.js
+++ front-end/src/store/provider/index.js
@@ -26,12 +26,14 @@ function Provider({ children }) {
   };
 
   const removeToCart = (item) => {
+    if (item.lenght === 1)
     setCart((old) => {
       let quantity = 0;
       let newCart;
       if (old[item.id]) {
         quantity = old[item.id].quantity;
       }
+      
       if (old[item.id] && old[item.id].quantity > 0) {
         newCart = {
           ...old,
