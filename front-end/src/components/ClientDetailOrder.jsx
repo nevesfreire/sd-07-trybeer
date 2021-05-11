90e02ff0f002b914119d5a46b1cf86b6fa71031e
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-05-11 17:07:46 -0300

    requisito 7 faltando implementaçoes de requisitos passados.

--- front-end/src/components/ClientDetailOrder.jsx
+++ front-end/src/components/ClientDetailOrder.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import dateFormat from 'dateformat';
-import { useHistory } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router';
 import { getDetailOrders } from '../services/Api/user';
 
 const ClientDetailOrder = () => {
@@ -16,34 +16,44 @@ const ClientDetailOrder = () => {
 
   useEffect(() => {
     const getClientOrders = async () => {
-      const { data: { productList, saleDetail } } = await getDetailOrders(idPathName);
+      const {
+        data: { productList, saleDetail },
+      } = await getDetailOrders(idPathName);
       setProducts(productList);
       setSaleInfo(saleDetail[0]);
       setLoading(false);
     };
     getClientOrders();
-    // getDetailOrdersidPathNamethen((r) => { setOrder(r); });
   }, [idPathName]);
+
+  if (!localStorage.getItem('token')) {
+    return (<Redirect to="/login" />);
+  }
 
   return (
     <div>
       <h1 data-testid="top-title">Detalhes de Pedido</h1>
-      {!loading
-        ? (
-          <div>
-            <h2 data-testid="order-number">{`Pedido  ${id} - ${status}`}</h2>
-            <h3 data-testid="order-date">{dateFormat(saleDate, 'dd/mm')}</h3>
-            {products.map(({ name, key, quantity, price }, index) => (
-              <div key={ key }>
-                <h5 data-testid={ `${index}-product-qtd` }>{quantity}</h5>
-                <h5 data-testid={ `${index}-product-name` }>{name}</h5>
-                <h5 data-testid={ `${index}-product-total-value` }>{`R$${quantity * price}`}</h5>
-              </div>
-            ))}
-            <h3 data-testid="order-total-value">{`Total: R$${totalPrice}`}</h3>
-          </div>
-        )
-        : 'deu merda'}
+      {!loading ? (
+        <div>
+          <h2 data-testid="order-number">{`Pedido  ${id} - ${status}`}</h2>
+          <h3 data-testid="order-date">{dateFormat(saleDate, 'dd/mm')}</h3>
+          {products.map(({ name, key, quantity, price }, index) => (
+            <div key={ key }>
+              <h5 data-testid={ `${index}-product-qtd` }>{quantity}</h5>
+              <h5 data-testid={ `${index}-product-name` }>{name}</h5>
+              <h5 data-testid={ `${index}-product-total-value` }>
+                {`R$${
+                  quantity * price
+                }`}
+
+              </h5>
+            </div>
+          ))}
+          <h3 data-testid="order-total-value">{`Total: R$${totalPrice}`}</h3>
+        </div>
+      ) : (
+        'deu merda'
+      )}
     </div>
   );
 };
