e0b3a07f0ba629f6a7e5439c7de7dc41bf975166
Fernando Freire <69821787+nevesfreire@users.noreply.github.com>
2021-05-11 22:09:59 -0300

    Revert "[ Grupo 12 ] Front details admin"

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -10,7 +10,6 @@ import AdminOrdersPage from '../pages/AdminOrdersPage';
 import CheckoutPage from '../pages/CheckoutPage';
 import HomePage from '../pages/HomePage';
 import DetailsPage from '../pages/DetailsPage';
-import SalesDetailsPage from '../pages/SalesDetailsPage';
 
 const Routes = () => (
   <BrowserRouter>
@@ -23,7 +22,6 @@ const Routes = () => (
     <Route exact path="/orders" component={ ClientOrdersPage } />
     <Route exact path="/checkout" component={ CheckoutPage } />
     <Route exact path="/orders/:id" component={ DetailsPage } />
-    <Route exact path="/admin/orders/:id" component={ SalesDetailsPage } />
     <Route exact path="/" component={ HomePage } />
   </BrowserRouter>
 );
