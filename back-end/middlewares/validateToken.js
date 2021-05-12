8345528e37ca5cb94f4635e2169e8b1781f0dcf2
Zeca Costa <jccostaso@gmail.com>
2021-05-12 16:07:57 -0300

    refact: back-end req6

--- back-end/middlewares/validateToken.js
+++ back-end/middlewares/validateToken.js
@@ -2,17 +2,17 @@ const jwt = require('jsonwebtoken');
 const login = require('../models/login');
 const httpStatus = require('../controllers/httpStatus');
 const secret = require('../controllers/login');
-// const secret = "meusegredoparajwt";
 
 const noTokenMessage = { message: 'Token não encontrado ou informado' };
 const userNotFound = { message: 'Erro ao procurar usuário do token.' };
 
 const validateToken = async (req, res, next) => { 
   const token = req.headers.authorization;
+  console.log(token)
   if (!token) return res.status(httpStatus.UNAUTHORIZED).json(noTokenMessage);
   try {
-    const decoded = jwt.verify(token, secret);
-    const user = await login.loginModel(decoded.data[1]);
+    const decoded = jwt.verify(token, secret.secret);
+    const user = await login.loginModel(decoded.email);
   if (!user) return res.status(httpStatus.UNAUTHORIZED).json(userNotFound);
       req.user = user;
       next();    
