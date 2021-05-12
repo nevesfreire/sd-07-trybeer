57c32833a2d17b9a21e5e30e8fc01e99777d5bc7
lisvalver <lisvalver.atte@gmail.com>
2021-05-12 18:18:48 -0300

    Estrutura base do req10 - falta as validacoes de admin

--- front-end/src/Pages/adm/AdmProfile.js
+++ front-end/src/Pages/adm/AdmProfile.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../../Components/Header';
 
 function AdmProfile() {
+  const { email, name } = JSON.parse(localStorage.getItem('user'));
+
+  if (!localStorage.getItem('user')) {
+    return <Redirect to="/login" />;
+  }
+
   return (
     <div>
-      <h1>ADM Profile</h1>
-      <Header />
+      <Header name="Perfil" />
+      <label htmlFor="profile-name">
+        Name
+        <input
+          readOnly
+          value={ name }
+          name="name"
+          data-testid="profile-name"
+          type="text"
+        />
+
+      </label>
+      <label htmlFor="profile-email">
+        Email
+        <input
+          readOnly
+          value={ email }
+          name="email"
+          data-testid="profile-email"
+          type="text"
+        />
+      </label>
     </div>
   );
 }
