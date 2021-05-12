fd7f269163cee12c622a7b63001ccef502a3c458
Andre Serudo <andreserudo@gmail.com>
2021-05-12 18:17:39 -0400

    corrigindo lint do merge

--- back-end/products/models/index.js
+++ back-end/products/models/index.js
@@ -6,17 +6,19 @@ const getAll = async () => {
   return data;
 };
 
-const createSale = async (idUser, totalProducts, street, houseNumber, status) => {
-  const query = 'INSERT INTO sales(user_id, total_price, delivery_address, delivery_number, sale_date, status) ' +
-  'VALUES (?, ?, ?, ?, NOW(), ?)';
+const createSale = async (idUser, totalProducts, street, houseNumber) => {
+  const status = 'Pendente';
+  const query = 'INSERT INTO '
+  + 'sales(user_id, total_price, delivery_address, delivery_number, sale_date, status) '
+  + 'VALUES (?, ?, ?, ?, NOW(), ?)';
   const [data] = await conn.execute(query, [idUser, totalProducts, street, houseNumber, status]);
   return data;
 };
 
-const createProductsSales = async (sale_id, product_id, quantity) => {
-  const query = 'INSERT INTO sales_products(sale_id, product_id, quantity) ' +
-  'VALUES (?, ?, ?)';
-  const [data] = await conn.execute(query, [sale_id, product_id, quantity]);
+const createProductsSales = async (saleId, productId, quantity) => {
+  const query = 'INSERT INTO sales_products(sale_id, product_id, quantity) '
+  + 'VALUES (?, ?, ?)';
+  const [data] = await conn.execute(query, [saleId, productId, quantity]);
   return data;
 };
 
