eb4232d83837fcf30399503814465ebae1deb519
josielvs <josielvs@gmail.com>
2021-05-13 14:54:45 -0300

    Requisito 11 FrontEnd Parcial

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 import { HeaderAdmin } from '../components';
+import { getOrders } from '../api';
+
+import ls from '../services';
 
 function Orders() {
+  const history = useHistory();
+
+  const getUserLogged = useCallback(async () => {
+    const dataUser = await ls.acessLocalStorage.getUserLocalStorage();
+    if (!dataUser) return history.push('/login');
+    const { token } = dataUser;
+    const getSales = await getOrders(token);
+    console.log(getSales);
+  }, [history]);
+
+  useEffect(() => {
+    getUserLogged();
+  }, [getUserLogged]);
+
   return (
-    <HeaderAdmin title="Admin - Pedidos" />
+    <div className="first-div">
+      <HeaderAdmin title="Admin - Pedidos" />
+      <h1><strong>Pedidos</strong></h1>
+    </div>
   );
 }
 
