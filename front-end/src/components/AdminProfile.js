70b379ae64d65bbf1a00f98dff3d706c07e6e353
Fabio Camargos <fabiocmmcm@gmail.com>
2021-05-13 00:39:06 -0300

    req 10 front

--- front-end/src/components/AdminProfile.js
+++ front-end/src/components/AdminProfile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ClientProfile() {
+export default function AdminProfile() {
   const currentUser = JSON.parse(localStorage.getItem('user'));
 
   return (
