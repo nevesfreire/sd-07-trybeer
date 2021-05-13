90531692af99ad497f2dddfb2567831e8c5dd039
Zeca Costa <jccostaso@gmail.com>
2021-05-13 19:21:27 -0300

    lint issues 2

--- back-end/models/checkoutModel.js
+++ back-end/models/checkoutModel.js
@@ -13,11 +13,14 @@ const createSale = async (data) => {
   const values = [userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status];
   await connect.execute(
     `INSERT INTO sales (user_id, total_price, delivery_address, delivery_number, sale_date, status)
-    VALUES(?, ?, ?, ?, ?, ?)`, values);
+    VALUES(?, ?, ?, ?, ?, ?)`, values,
+  );
 };
 
 const productPrice = async (productId) => {
-  const [[{ price }]] = await connect.execute('SELECT price FROM products WHERE id = ?', [productId]);
+  const [[{ price }]] = await connect.execute(
+    'SELECT price FROM products WHERE id = ?', [productId]
+  );
   return price;
 };
 
