ccd8ac57c0dc6a803b23d49327f93cb94930e040
MoisesSantana <moisaant@gmail.com>
2021-05-13 18:20:32 -0300

    correcao localStorage e state

--- front-end/src/components/ProductCardComponent.jsx
+++ front-end/src/components/ProductCardComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from '../context/TrybeerContext';
 
@@ -20,6 +20,13 @@ function ProductCard({ product, index }) {
     setQuantity(quantity + 1);
     addInCart(id, name, price);
   };
+
+  useEffect(() => {
+    const orderResult = JSON.parse(localStorage.getItem('products'));
+    const quantityById = orderResult.find((object) => object.id === product.id);
+    if (!quantityById) return setQuantity(0);
+    setQuantity(quantityById.quantity);
+  }, []);
 
   return (
     <div key={ product.id }>
