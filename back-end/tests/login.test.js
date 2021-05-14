f34bf996b33e3e5789217f4612beb69a69e38513
Flavio Oliveira <flavsoliver@gmail.com>
2021-05-14 15:14:53 -0300

    refatoração de lint

--- back-end/tests/login.test.js
+++ back-end/tests/login.test.js
@@ -1,38 +1,30 @@
 const frisby = require('frisby');
 
 const {
+  // ReasonPhrases,
+  StatusCodes,
+  // getReasonPhrase,
+  // getStatusCode,
+} = require('http-status-codes');
+const {
   createTableUsers,
   deleteTableUsers,
   insertUsers,
   autoIncrementTableUsers,
-} = require('../utils/testsSupport')
-
-const {
-  ReasonPhrases,
-  StatusCodes,
-  getReasonPhrase,
-  getStatusCode,
-} =  require('http-status-codes');
+} = require('../utils/testsSupport');
 
 const url = 'http://localhost:3001';
 
-describe('Teste no endpoint de login', () => {
-  beforeEach(() => {
-    createTableUsers();
-  });
-
-  afterEach(() => {
-    deleteTableUsers();
-    autoIncrementTableUsers();
-  });
-
+describe('Teste no endpoint de login com usuário inexistente', () => {
+  beforeEach(() => { createTableUsers(); });
+  afterEach(() => { deleteTableUsers(); autoIncrementTableUsers(); });
   it('Verifica se é possível fazer login com usuário inexistente', async () => {
     insertUsers();
     await frisby
       .post(`${url}/user/login`,
         {
           email: 'fulano@mail.com',
-          password: '123456'
+          password: '123456',
         })
       .expect('status', StatusCodes.NOT_FOUND)
       .then((response) => {
@@ -42,14 +34,18 @@ describe('Teste no endpoint de login', () => {
         expect(result.message).toBe('usuário ou senha incorreto');
       });
   });
+});
 
+describe('Teste no endpoint de login com senha de usuário incorreta', () => {
+  beforeEach(() => { createTableUsers(); });
+  afterEach(() => { deleteTableUsers(); autoIncrementTableUsers(); });
   it('Verifica se é possível fazer login com senha de usuário incorreta', async () => {
     insertUsers();
     await frisby
       .post(`${url}/user/login`,
         {
           email: 'tryber@trybe.com.br',
-          password: 'senha123456'
+          password: 'senha123456',
         })
       .expect('status', StatusCodes.NOT_FOUND)
       .then((response) => {
@@ -59,14 +55,18 @@ describe('Teste no endpoint de login', () => {
         expect(result.message).toBe('usuário ou senha incorreto');
       });
   });
+});
 
+describe('Teste no endpoint de login com usuário existente e senha correta', () => {
+  beforeEach(() => { createTableUsers(); });
+  afterEach(() => { deleteTableUsers(); autoIncrementTableUsers(); });
   it('Verifica se é possível fazer login com usuário existente e senha correta', async () => {
     insertUsers();
     await frisby
       .post(`${url}/user/login`,
         {
           email: 'tryber@trybe.com.br',
-          password: '123456'
+          password: '123456',
         })
       .expect('status', StatusCodes.OK)
       .then((response) => {
@@ -76,4 +76,4 @@ describe('Teste no endpoint de login', () => {
         console.log('result', result);
       });
   });
-})
+});
--- back-end/utils/testsSupport.js
+++ back-end/utils/testsSupport.js
@@ -3,30 +3,30 @@ function createDataBase() {
 }
 
 function createTableUsers() {
-  const createTableUsers = `CREATE TABLE IF NOT EXISTS Trybeer.users (
+  const create = `CREATE TABLE IF NOT EXISTS Trybeer.users (
     id INT NOT NULL AUTO_INCREMENT,
       name VARCHAR(100) NOT NULL,
       email VARCHAR(100) NOT NULL,
       password VARCHAR(20) NOT NULL,
       role VARCHAR(20) NOT NULL,
       PRIMARY KEY (id),
-      UNIQUE KEY \`email_un\` (email));`
-  return createTableUsers;
+      UNIQUE KEY \`email_un\` (email));`;
+  return create;
 }
 
-function insertUsers(){
-  const insertUsers = `INSERT INTO Trybeer.users (id, name, email, password, role)
+function insertUsers() {
+  const insert = `INSERT INTO Trybeer.users (id, name, email, password, role)
       VALUES
       ('1', 'Tryber Admin', 'tryber@trybe.com.br', '123456', 'administrator'),
-      ('2', 'testuser', 'user@test.com', 'test123', 'client');`
-  return insertUsers;
+      ('2', 'testuser', 'user@test.com', 'test123', 'client');`;
+  return insert;
 }
 
-function deleteTableUsers(){
+function deleteTableUsers() {
   return 'DELETE FROM Trybeer.users;';
 }
 
-function autoIncrementTableUsers(){
+function autoIncrementTableUsers() {
   return 'ALTER TABLE Trybeer.users AUTO_INCREMENT = 1;';
 }
 
@@ -36,11 +36,7 @@ module.exports = {
   insertUsers,
   deleteTableUsers,
   autoIncrementTableUsers,
-}
-
-
-
-
+};
 
 // 'DELETE FROM Trybeer.sales_products;';
 // 'ALTER TABLE Trybeer.sales_products AUTO_INCREMENT = 1;';
