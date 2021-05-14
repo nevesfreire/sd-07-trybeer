a69d72cec8f4d809447a97c14ffc426fac466433
muriloPereira006 <muriloedu006@gmail.com>
2021-05-14 15:31:47 -0300

    requisito 7 ok!

--- front-end/src/pages/orders/index.js
+++ front-end/src/pages/orders/index.js
@@ -7,48 +7,70 @@ import './style.css';
 
 export default function Orders() {
   const history = useHistory();
-  const { userLogged } = useContext(TrybeerContext);
+  const {
+    userLogged,
+    setSaleId,
+    setSaleDate,
+    setTotalPrice,
+  } = useContext(TrybeerContext);
   const [ordersFromUser, setOrdersFromUser] = useState([]);
 
-  const listOrdes = async () => {
-    const orders = await getSaleByUserId(userLogged.id);
-    setOrdersFromUser(orders);
-    console.log(orders);
+  useEffect(() => {
+    const listOrdes = async () => {
+      const orders = await getSaleByUserId(userLogged.id);
+      setOrdersFromUser(orders);
+      console.log(orders);
+    };
+    listOrdes();
+  }, [userLogged.id]);
+
+  const options = {
+    day: '2-digit', month: '2-digit',
   };
 
-  useEffect(() => {
-    listOrdes();
-  }, []);
+  const handleClick = (id, date, price) => {
+    setSaleId(id);
+    setSaleDate(date);
+    setTotalPrice(price);
+    history.push(`/orders/${id}`);
+  };
 
   return (
     <div>
       <TopMenu topTitle="Meus Pedidos" />
-      <h6>{userLogged.name}</h6>
-      <h6>{userLogged.email}</h6>
-      <h6>{userLogged.role}</h6>
-      <h6>{userLogged.id}</h6>
       <div>
-        {ordersFromUser.map((order) => (
-          <button
-            type="button"
-            className="cardOrder"
-            key={ order.id - 1 }
-            data-testid={ `${order.id - 1}-order-card-container` }
-            onClick={ () => history.push(`/orders/${order.id}`) }
-          >
-            <span
-              data-testid={ `${order.id - 1}-order-number` }
+        {ordersFromUser.map((order, index) => {
+          const priceOrder = new Intl
+            .NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+            .format(order.total_price);
+          const dateOrder = new Intl.DateTimeFormat('pt-BR', options)
+            .format(Date.parse(order.sale_date));
+          return (
+            <button
+              type="button"
+              className="cardOrder"
+              key={ order.id }
+              data-testid={ `${index}-order-card-container` }
+              onClick={ () => handleClick(order.id, dateOrder, priceOrder) }
             >
-              Pedido {order.id}
-            </span>
-            <h2 data-testid={ `${order.id - 1}-order-date` }>{order.sale_date}</h2>
-            <h2
-              data-testid={ `${order.id - 1}-order-total-value` }
-            >
-              {order.total_price}
-            </h2>
-          </button>
-        ))}
+              <span
+                data-testid={ `${index}-order-number` }
+              >
+                {`Pedido ${order.id} `}
+              </span>
+              <h2
+                data-testid={ `${index}-order-date` }
+              >
+                {dateOrder}
+              </h2>
+              <h2
+                data-testid={ `${index}-order-total-value` }
+              >
+                {priceOrder}
+              </h2>
+            </button>
+          );
+        })}
       </div>
     </div>
   );
