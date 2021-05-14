3551e80822f2cdfc32f6e7b942258ee66ed1cd76
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-05-14 14:32:27 -0300

    tests req4 e req-7

--- back-end/test/api/tstHelper/connection.js
+++ back-end/test/api/tstHelper/connection.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.HOSTNAME || '127.0.0.1',
-  user: process.env.MYSQL_USER || 'root', 
-  password: process.env.MYSQL_PASSWORD ?? '12345',
+  host: process.env.HOSTNAME ,
+  user: process.env.MYSQL_USER ,
+  password: process.env.MYSQL_PASSWORD,
   database: 'Trybeer',
 });
 
