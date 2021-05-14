605398e973a8167e5a1190091daae67e072120c4
muriloPereira006 <muriloedu006@gmail.com>
2021-05-14 02:21:58 -0300

    necessario tratar retorno de data

--- front-end/src/pages/orders/index.js
+++ front-end/src/pages/orders/index.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { TopMenu } from '../../components';
+import TrybeerContext from '../../context/TrybeerContext';
+import { getSaleByUserId } from '../../service/trybeerApi';
+import './style.css';
 
 export default function Orders() {
+  const history = useHistory();
+  const { userLogged } = useContext(TrybeerContext);
+  const [ordersFromUser, setOrdersFromUser] = useState([]);
+
+  const listOrdes = async () => {
+    const orders = await getSaleByUserId(userLogged.id);
+    setOrdersFromUser(orders);
+    console.log(orders);
+  };
+
+  useEffect(() => {
+    listOrdes();
+  }, []);
+
   return (
     <div>
       <TopMenu topTitle="Meus Pedidos" />
-      ORDERS PAGE
+      <h6>{userLogged.name}</h6>
+      <h6>{userLogged.email}</h6>
+      <h6>{userLogged.role}</h6>
+      <h6>{userLogged.id}</h6>
+      <div>
+        {ordersFromUser.map((order) => (
+          <button
+            type="button"
+            className="cardOrder"
+            key={ order.id - 1 }
+            data-testid={ `${order.id - 1}-order-card-container` }
+            onClick={ () => history.push(`/orders/${order.id}`) }
+          >
+            <span
+              data-testid={ `${order.id - 1}-order-number` }
+            >
+              Pedido {order.id}
+            </span>
+            <h2 data-testid={ `${order.id - 1}-order-date` }>{order.sale_date}</h2>
+            <h2
+              data-testid={ `${order.id - 1}-order-total-value` }
+            >
+              {order.total_price}
+            </h2>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
