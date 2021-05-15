e494a24dc78f44438d7311137999efccafc21191
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-05-15 10:11:45 -0300

    Centralizando a tela de registro

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import CreateUserForm from '../../components/CreateUserForm';
 // import CreateUserForm from '../../components/CreateUserForm';
 
 const Register = () => (
-  <div>
+  <Container fluid style={ { width: '450px', heigth: '100vh' } }>
     <h1>Registro</h1>
     <div>
       <CreateUserForm />
     </div>
-  </div>
+  </Container>
 );
 
 export default Register;
