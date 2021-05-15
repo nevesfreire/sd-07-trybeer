aa537e1084fad64c2678a46bd8d653064775f699
Filipi Firmino <contatoic3web@gmail.com>
2021-05-14 23:55:29 -0300

    Acesso a orders apenas logado

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import OrderCard from '../components/OrderCard';
 import Menu from '../components/Menu';
 import Header from '../components/Header';
@@ -9,10 +10,15 @@ const Orders = () => {
   const URL = '/orders';
 
   const [orders, setOrders] = useState([
-    { id: 1, delivery_number: '1', sale_date: '10-12-2011', total_price: '100' },
+    {
+      id: 1,
+      delivery_number: '1',
+      sale_date: '10-12-2011',
+      total_price: '100',
+    },
   ]);
 
-  const loggedUser = JSON.parse(localStorage.getItem('data'));
+  const loggedUser = JSON.parse(localStorage.getItem('data')) || { id: null };
   const params = {
     headers: { 'content-type': 'application/json' },
     id: loggedUser.id,
@@ -31,21 +37,28 @@ const Orders = () => {
 
   return (
     <div className="container">
-      <Header title="Meus Pedidos" />
-      <div className="container-int">
-        <Menu />
-        <div className="myOrder-body">
-          {orders.map((actual, index) => (
-            <OrderCard
-              position={ index }
-              key={ actual.delivery_number }
-              deliveryNumber={ actual.delivery_number }
-              saleDate={ actual.sale_date }
-              totalPrice={ actual.total_price }
-            />
-          ))}
+      {loggedUser.id === null ? (
+        <Redirect to="/login" />
+      ) : (
+        <div>
+          <Header title="Meus Pedidos" />
+          <div className="container-int">
+            <Menu />
+            <div className="myOrder-body">
+              {orders.map((actual, index) => (
+                <OrderCard
+                  position={ index }
+                  key={ actual.delivery_number }
+                  deliveryNumber={ actual.delivery_number }
+                  saleDate={ actual.sale_date }
+                  totalPrice={ actual.total_price }
+                />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
+      ;
     </div>
   );
 };
