48c537088c0d6e59ca4f27471a7e582088c8e760
Pedro Risso <pmerchiorattor@gmail.com>
2021-05-15 12:57:59 -0300

    fix: tests and login route

--- back-end/services/loginServ.js
+++ back-end/services/loginServ.js
@@ -1,23 +1,27 @@
 const jwt = require('jsonwebtoken');
-
-const { wrongPassword, dbSearchReturnedEmpty, errorInDb } = require('./dictionaries/statusMsgMap');
+const preCheckFields = require('./helpers/validateFields');
+const { allFieldsMustBeFilled,
+  wrongPassword,
+  dbSearchReturnedEmpty,
+  errorInDb } = require('./dictionaries/statusMsgMap');
 const { getUserByEmail } = require('../models/userModels');
 
-const checkUser = async (email) => {
+const checkUser = async (body) => {
+  if (!preCheckFields(['email', 'password'], [body])) return allFieldsMustBeFilled;
+  const { email, password } = body;
   const userInDb = await getUserByEmail(email);
-  if (!userInDb) return null;
+  if (!userInDb) return dbSearchReturnedEmpty;
+  if (password !== userInDb.password) return wrongPassword;
   if (userInDb.err) return errorInDb;
   return userInDb;
 };
 
 const loginServ = async (body) => {
   try {
-    const { email, password } = body;
-    const checkUserRes = await checkUser(email);
-    if (!checkUserRes) return dbSearchReturnedEmpty;
+    const checkUserRes = await checkUser(body);
+    if (checkUserRes.error) return checkUserRes;
+    const { email } = body;
     const { id, name, role } = checkUserRes;
-    if (password !== checkUserRes.password) return wrongPassword;
-
     const payload = { id, role };
     const token = jwt.sign(payload, process.env.SECRET || '12345');
     const msgRes = { name, email, role, token };
