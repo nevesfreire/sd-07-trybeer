20c099fcfaf5b1928fa8db944c7e7de108554d66
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-05-15 08:24:21 -0300

    Adicionando imagem ao login

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import LoginForm from '../../components/LoginForm';
+import './login.css';
 
 const Login = () => (
-  <Container fluid="md">
-    <h1>Login</h1>
-    <LoginForm />
-  </Container>
+  <div className="login">
+    <Container
+      fluid
+      style={ {
+        height: '100vh',
+        paddingTop: '150px',
+        paddingLeft: '60px',
+        width: '70vh',
+      } }
+    >
+      <h1>Login</h1>
+      <LoginForm />
+    </Container>
+  </div>
 );
 
 export default Login;
