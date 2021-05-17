e8fc9d003b382e9ec21bb624aa91d489a01dac75
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-05-16 23:52:16 -0300

    alter date for CURRENT_DATE() - 1

--- back-end/src/models/SalesProduct.js
+++ back-end/src/models/SalesProduct.js
@@ -6,7 +6,8 @@ const getSalesProductByUserId = async (id) => {
     FROM Trybeer.sales_products AS sp
     INNER JOIN Trybeer.sales AS s ON s.user_id = ${id}
     INNER JOIN Trybeer.products AS p ON p.id = sp.product_id
-    GROUP BY s.id;`,
+    GROUP BY s.id
+    ORDER BY s.id ASC;`,
   );
   return data;
 };
@@ -16,12 +17,12 @@ const createSalesProductBySalesIdAndProductId = async (
   productId,
   quantity,
 ) => {
-  const [[data]] = await connection.execute(
+  const [data] = await connection.execute(
     `INSERT INTO sales_products (sale_id, product_id, quantity)
     VALUES (${saleId}, ${productId}, ${quantity})`,
   );
     console.log(data);
-  return data;
+  return data[0];
 };
 
 module.exports = {
