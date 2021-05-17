2e4c2aff3fc11fd89b89b4f5d23a59923e692c9e
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-05-17 00:33:45 -0300

    resolving date

--- back-end/src/models/Sales.js
+++ back-end/src/models/Sales.js
@@ -7,7 +7,7 @@ const savePurchases = async (objSales) => {
           user_id, total_price, delivery_address, delivery_number, sale_date, status) 
               VALUES ("${objSales.userId}","${objSales.totalPrice}"
               ,"${objSales.deliveryAddress}","${objSales.deliveryNumber}"
-              ,CURRENT_DATE() - 1, "${objSales.status}")`,
+              ,CURRENT_DATE(), "${objSales.status}")`,
   );
   return sales;
 };
