970ad7acd8528b1c153e50f2d062ad118b22e003
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-06-10 13:07:17 -0300

    Adaptado o teste user, falhou na criação do usuario, o id não está sendo retornado

--- back-end/tests/user.test.js
+++ back-end/tests/user.test.js
@@ -1,7 +1,8 @@
 const request = require('supertest');
 const express = require('express');
 const { user } = require('../routes');
-const connect = require('../models/connection');
+const db = require('../models');
+const shell = require('shelljs');
 const message = require('./config/errorMessages');
 const { users, contentType, applicationJson, tokens } = require('./config/parameters');
 
@@ -14,45 +15,57 @@ const validEmail = 'teste@teste.com';
 const validPassword = '1234567';
 const validRole = 'client';
 
-beforeAll(async () => connect.execute('DELETE FROM Trybeer.users WHERE email = \'new@user.com\''));
+beforeAll(async () => {
+  await db.sequelize.sync({ force: true });
+  shell.exec('npx sequelize db:seed:all');
+});
 
-it('Não é possível cadastrar um usuário sem o campo name', (done) => request(app)
+it('Não é possível cadastrar um usuário sem o campo name', (done) =>
+  request(app)
     .post('/user')
     .send({})
     .set('Accept', applicationJson)
     .expect(contentType, /json/)
     .expect(400, message.nameRequired, done));
-  
-it('Não é possível cadastrar um usuário sem o campo email', (done) => request(app)
-  .post('/user')
-  .send({ name: validName })
-  .set('Accept', applicationJson)
-  .expect(contentType, /json/)
-  .expect(400, message.emailRequired, done));
 
-it('Não é possível cadastrar um usuário sem o campo password', (done) => request(app)
-  .post('/user')
-  .send({ name: validName, email: validEmail })
-  .set('Accept', applicationJson)
-  .expect(contentType, /json/)
-  .expect(400, message.passwordIsRequired, done));
-
-it('Não é possível cadastrar um usuário sem o campo role', (done) => request(app)
-  .post('/user')
-  .send({ name: validName, email: validEmail, password: validPassword })
-  .set('Accept', applicationJson)
-  .expect(contentType, /json/)
-  .expect(400, message.roleIsRequired, done));
-
-it('Não é possível cadastrar um com usuário o campo name contendo numeros ou simbulos', (done) => 
+it('Não é possível cadastrar um usuário sem o campo email', (done) =>
   request(app)
     .post('/user')
-    .send({ name: 'Joao Amaral 1940', email: validEmail, password: validPassword, role: validRole })
+    .send({ name: validName })
+    .set('Accept', applicationJson)
+    .expect(contentType, /json/)
+    .expect(400, message.emailRequired, done));
+
+it('Não é possível cadastrar um usuário sem o campo password', (done) =>
+  request(app)
+    .post('/user')
+    .send({ name: validName, email: validEmail })
+    .set('Accept', applicationJson)
+    .expect(contentType, /json/)
+    .expect(400, message.passwordIsRequired, done));
+
+it('Não é possível cadastrar um usuário sem o campo role', (done) =>
+  request(app)
+    .post('/user')
+    .send({ name: validName, email: validEmail, password: validPassword })
+    .set('Accept', applicationJson)
+    .expect(contentType, /json/)
+    .expect(400, message.roleIsRequired, done));
+
+it('Não é possível cadastrar um com usuário o campo name contendo numeros ou simbulos', (done) =>
+  request(app)
+    .post('/user')
+    .send({
+      name: 'Joao Amaral 1940',
+      email: validEmail,
+      password: validPassword,
+      role: validRole,
+    })
     .set('Accept', applicationJson)
     .expect(contentType, /json/)
     .expect(400, /fails to match the required pattern/, done));
 
-it('Não é possível cadastrar um com usuário o campo name menor que 12 caracteres', (done) => 
+it('Não é possível cadastrar um com usuário o campo name menor que 12 caracteres', (done) =>
   request(app)
     .post('/user')
     .send({ name: 'Joao', email: validEmail, password: validPassword, role: validRole })
@@ -60,14 +73,20 @@ it('Não é possível cadastrar um com usuário o campo name menor que 12 caract
     .expect(contentType, /json/)
     .expect(400, message.invalidNameLength, done));
 
-it('Não é possível cadastrar um usuário com o campo email inválido', (done) => request(app)
-  .post('/user')
-  .send({ name: validName, email: 'teste@teste', password: validPassword, role: validRole })
-  .set('Accept', applicationJson)
-  .expect(contentType, /json/)
-  .expect(400, message.invalidEmailType, done));
+it('Não é possível cadastrar um usuário com o campo email inválido', (done) =>
+  request(app)
+    .post('/user')
+    .send({
+      name: validName,
+      email: 'teste@teste',
+      password: validPassword,
+      role: validRole,
+    })
+    .set('Accept', applicationJson)
+    .expect(contentType, /json/)
+    .expect(400, message.invalidEmailType, done));
 
-it('Não é possível cadastrar um usuário com uma senha menor que 6 caracteres', (done) => 
+it('Não é possível cadastrar um usuário com uma senha menor que 6 caracteres', (done) =>
   request(app)
     .post('/user')
     .send({ name: validName, email: validEmail, password: '1234', role: validRole })
@@ -75,7 +94,7 @@ it('Não é possível cadastrar um usuário com uma senha menor que 6 caracteres
     .expect(contentType, /json/)
     .expect(400, message.invalidPasswordLength, done));
 
-it('Não é possível cadastrar um usuário que a role não seja administrator ou client', (done) => 
+it('Não é possível cadastrar um usuário que a role não seja administrator ou client', (done) =>
   request(app)
     .post('/user')
     .send({ name: validName, email: validEmail, password: validPassword, role: 'outro' })
@@ -83,7 +102,7 @@ it('Não é possível cadastrar um usuário que a role não seja administrator o
     .expect(contentType, /json/)
     .expect(400, message.invalidRoleName, done));
 
-it('Não é possível cadastrar um email que já existe no banco', (done) => 
+it('Não é possível cadastrar um email que já existe no banco', (done) =>
   request(app)
     .post('/user')
     .send(users.tryberAdmin)
@@ -91,7 +110,7 @@ it('Não é possível cadastrar um email que já existe no banco', (done) =>
     .expect(contentType, /json/)
     .expect(400, message.emailTaken, done));
 
-it(' É possível cadastrar um novo usuario', (done) => 
+it(' É possível cadastrar um novo usuario', (done) =>
   request(app)
     .post('/user')
     .send(users.newUser)
@@ -104,15 +123,15 @@ it(' É possível cadastrar um novo usuario', (done) =>
       done();
     }));
 
-it('Não é possível atualizar o nome do usuario sem um token valido.', (done) => 
+it('Não é possível atualizar o nome do usuario sem um token valido.', (done) =>
   request(app)
     .put('/user')
     .send({ name: 'Testando Novo Nome' })
     .set('Accept', applicationJson)
     .expect(contentType, /json/)
     .expect(401, /jwt must be provided/, done));
- 
-it('Deve ser possivel atualizar o nome do usuario.', (done) => 
+
+it('Deve ser possivel atualizar o nome do usuario.', (done) =>
   request(app)
     .put('/user')
     .send({ name: 'Testando Novo Usuario' })
@@ -121,7 +140,7 @@ it('Deve ser possivel atualizar o nome do usuario.', (done) =>
     .expect(contentType, /json/)
     .expect(200, done));
 
-it('Caso seja o mesmo nome, o usuario não deve ser atualizado.', (done) => 
+it('Caso seja o mesmo nome, o usuario não deve ser atualizado.', (done) =>
   request(app)
     .put('/user')
     .send({ name: 'Testando Novo Usuario' })
@@ -129,7 +148,7 @@ it('Caso seja o mesmo nome, o usuario não deve ser atualizado.', (done) =>
     .set('Accept', applicationJson)
     .expect(304, done));
 
-it('O nome do usuario não pode conter numeros ou caracteres especiais.', (done) => 
+it('O nome do usuario não pode conter numeros ou caracteres especiais.', (done) =>
   request(app)
     .put('/user')
     .send({ name: 'Testando Novo 1' })
@@ -139,6 +158,5 @@ it('O nome do usuario não pode conter numeros ou caracteres especiais.', (done)
     .expect(400, /fails to match the required pattern/, done));
 
 afterAll(async () => {
-  await connect.execute('UPDATE Trybeer.users SET name = "testuser" WHERE email = "user@test.com"');
-  return connect.end();
+  await db.sequelize.close();
 });
